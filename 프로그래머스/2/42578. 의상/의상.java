082d166355bd6cc64a99c7d75bd4d97a04122864
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-11 20:42:15 +0900

    [level 2] Title: 의상, Time: 2.88 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -5,14 +5,12 @@ class Solution {
         int answer = 1;
         
         Map<String, Integer> map = new HashMap<>();
-        
         for(int i=0; i<clothes.length; i++){
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        for(String key : map.keySet()){
-            int value = map.get(key);
-            answer *= (value + 1);
+        for(String s : map.keySet()){
+            answer *= (map.get(s) + 1);
         }
         
         return answer - 1;
