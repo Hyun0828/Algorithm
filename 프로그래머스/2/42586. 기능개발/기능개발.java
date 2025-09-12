0dca12c60bc2a54077b30d6b4f75f1bfee33b2cc
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-12 09:13:38 +0900

    [level 2] Title: 기능개발, Time: 0.58 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,48 +1,35 @@
 import java.util.*;
 
 class Solution {
-    
-    private List<Integer> days = new ArrayList<>();
-    
     public int[] solution(int[] progresses, int[] speeds) {
-        init(progresses, speeds);
-        return getResult();
-    }
-    
-    public void init(int[] progresses, int[] speeds){
-        int len = progresses.length;
+        List<Integer> answer = new ArrayList<>();
         
-        for(int i=0; i<len; i++){
-            int progress = progresses[i];
-            int speed = speeds[i];
-            int day = (100 - progress) / speed;
-            
-            if((100 - progress) % speed != 0)
-                day += 1;
-            
-            days.add(day);
+        Stack<Integer> stack = new Stack<>();
+        for(int i=progresses.length-1; i>=0; i--){
+            int day = (int)Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+            stack.push(day);
         }
-    }
-    
-    public int[] getResult(){
-        List<Integer> result = new ArrayList<>();
         
-        for(int i=0; i<days.size(); i++){
-            int day = days.get(i);
-            int j = i + 1;
-            int count = 1;
-            
-            while(j < days.size() && days.get(j) <= day){
+        int day = stack.pop();
+        int count = 1;
+        while(!stack.isEmpty()){
+            if(day >= stack.peek()){
                 count++;
-                j++;
+                stack.pop();
+            } else {
+                answer.add(count);
+                day = stack.pop();
+                count = 1;
             }
-            
-            result.add(count);
-            i = j - 1;
         }
         
-        return result.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        if(count >= 1)
+            answer.add(count);
+        
+        int[] result = new int[answer.size()];
+        for(int i=0; i<result.length; i++)
+            result[i] = answer.get(i);
+        
+        return result;
     }
 }
