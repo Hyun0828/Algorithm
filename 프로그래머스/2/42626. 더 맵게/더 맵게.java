05ef5ced9baad71258724d478538dbdecb45ab32
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-13 09:50:26 +0900

    [level 2] Title: 더 맵게, Time: 1593.98 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,42 +1,30 @@
 import java.util.*;
 
 class Solution {
-    
-    int answer = 0;
-    PriorityQueue<Integer> heap = new PriorityQueue<>();  
-    
     public int solution(int[] scoville, int K) {
-        // 최소힙에 다 넣고
-        insert(scoville);
-        // 하나씩 꺼내면서 K보다 작은 스코빌 음식이 2개 꺼내지면 더하고, K보다 작으면 다시 힙에 추가
-        if(solve(K))
-            return answer;
-        else
-            return -1;
-    }
-    
-    private void insert(int[] scoville){
-        for(int s : scoville)
-            heap.add(s);
-    }
-    
-    private boolean solve(int K){
+        int answer = 0;
         
-        boolean flag = true;
-        
-        while(heap.peek() < K){
-            
-            if(heap.size() >= 2){
-                int mix = heap.poll() + 2 * heap.poll();
-                heap.add(mix);
-                answer++;    
-            }
-            else if(heap.size() <= 1){
-                flag = false;
-                break;
-            }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<scoville.length; i++){
+            pq.add(scoville[i]);
         }
         
-        return flag;
+        while(!pq.isEmpty()){
+            int least = pq.peek();
+            if(least >= K){
+                break;
+            }
+            
+            int a = pq.poll();
+            if(pq.isEmpty()){
+                answer = -1;
+                break;
+            }
+            int b = pq.poll();
+            pq.add(a+b*2);
+            answer++;
+        }
+        
+        return answer;
     }
 }
