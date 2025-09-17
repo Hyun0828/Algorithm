fd452600098bd724ee5b5b30c6e4beb4263e52e8
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-17 20:47:53 +0900

    [level 2] Title: 소수 찾기, Time: 26.06 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,45 +2,35 @@ import java.util.*;
 
 class Solution {
     
-    static Set<Integer> sVisited;
-    static boolean[] visited;
-    static char[] arr;
-    static int len;
-    static int answer;
+    int answer = 0;
+    Set<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
-        sVisited = new HashSet<>();
-        answer = 0;
-        arr = numbers.toCharArray();
-        visited = new boolean[numbers.length()];
-        len = arr.length;
-        
-        permutation(0, "");
+        permutation(numbers.toCharArray(), new boolean[numbers.length()], "");
         return answer;
     }
     
-    public static void permutation(int cnt, String s){
-        if(cnt > len){
+    public void permutation(char[] numbers, boolean[] visited, String number){
+        if(!number.equals("") && isPrime(Integer.parseInt(number)) && !set.contains(Integer.parseInt(number))){
+            answer++;
+            set.add(Integer.parseInt(number));
+        }
+        
+        if(number.length() == numbers.length){
             return;
         }
         
-        if(cnt >= 1 && !sVisited.contains(Integer.parseInt(s)) && isPrime(Integer.parseInt(s))){
-            sVisited.add(Integer.parseInt(s));
-            answer++;
-        }
-        
-        for(int i=0; i<len; i++){
+        for(int i=0; i<numbers.length; i++){
             if(visited[i])
                 continue;
-            
             visited[i] = true;
-            permutation(cnt+1, s+arr[i]);
+            permutation(numbers, visited, number+numbers[i]);
             visited[i] = false;
         }
     }
     
-    public static boolean isPrime(int n){
-        if(n<=1)
+    public boolean isPrime(int n){
+        if(n <= 1)
             return false;
         
         for(int i=2; i<=Math.sqrt(n); i++){
