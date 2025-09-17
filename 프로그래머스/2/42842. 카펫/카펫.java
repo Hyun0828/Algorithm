400c7cebcdf45366aa73493ec9c58b37bf38c1df
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-17 20:58:16 +0900

    [level 2] Title: 카펫, Time: 8.55 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,15 +4,26 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int sum = (brown+4)/2;
-        int mul = brown+yellow;
-        int sub = (int)Math.sqrt(sum*sum-4*mul);
+        // 가로(w), 세로(h)
+        // brown = 2*(w+h) - 4;
+        // yellow = w*h - brown;
         
-        int width = Math.max((sum+sub)/2, (sum-sub)/2);
-        int height = Math.min((sum+sub)/2, (sum-sub)/2);
+        // brown/2+2 = w+h
+        // brown+yellow = wh
         
-        answer[0] = width;
-        answer[1] = height;
+        int m = brown+yellow;
+        for(int i=m; i>=0; i--){
+            if(m % i == 0){
+                int w = Math.max(m/i, i);
+                int h = Math.min(m/i, i);
+                
+                if(w+h == brown/2 + 2){
+                    answer[0] = w;
+                    answer[1] = h;
+                    break;
+                }
+            }
+        }
         
         return answer;
     }
