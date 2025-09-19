893664cd1341de16424c232135837eccf5e9d83a
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-19 19:19:49 +0900

    [level 2] Title: 구명보트, Time: 11.36 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,20 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people);
         
-        // greedy : 최대한 꽉 채워서 보내자
+        // limit를 최대한 꽉 채워서 사람을 태우는 게 목표
+        // 몸무게가 가장 큰 사람 + 그 외 1명 채우기 (Upper Bound)
+        Arrays.sort(people);
         int left = 0;
         int right = people.length - 1;
         
         while(left <= right){
             if(people[left] + people[right] <= limit){
+                answer++;
                 left++;
                 right--;
-                answer++;
             } else {
-                right--;
                 answer++;
+                right--;
             }
         }
         
