60379147f2f07ee0252132ad0336abd97a24e2fc
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-25 16:39:12 +0900

    [level 2] Title: [1차] 캐시, Time: 130.00 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -4,41 +4,50 @@ class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
         
+        // 캐시 자료구조 : Map<String, Integer>, cache miss일 때는 Integer 값이 가장 작은 key를 제거한다.
+        // cache hit면 Integer 값을 업데이트 하는데, 도시의 index로
         for(int i=0; i<cities.length; i++){
             cities[i] = cities[i].toUpperCase();
         }
+    
+        Map<String, Integer> cache = new HashMap<>();
         
-        if(cacheSize > 0){
-            
-            Queue<String> queue = new LinkedList<>();
-            for(int i=0; i<cities.length; i++){
-                String city = cities[i];
-                if(!queue.isEmpty()){
-                    // 캐싱
-                    if(queue.contains(city)){
-                        queue.remove(city);
-                        queue.add(city);
-                        answer++;
-                    } else {
-                        // 캐시가 꽉차면 교체한다.
-                        if(queue.size() == cacheSize){
-                            queue.poll();
-                            queue.add(city);
-                            answer += 5;
-                        } else if(queue.size() < cacheSize){
-                            queue.add(city);
-                            answer += 5;
-                        }
-                    }
+        if(cacheSize == 0) {
+            return cities.length * 5;  // 전부 cache miss
+        }
+        
+        for(int i=0; i<cities.length; i++){
+            // cache hit
+            if(cache.containsKey(cities[i])){
+                cache.put(cities[i], i);
+                answer++;
+            }
+            // cache miss
+            else {
+                if(cache.size() < cacheSize){
+                    cache.put(cities[i], i);
+                    answer += 5;    
                 } else {
-                    queue.add(city);
+                    cacheMiss(cache, cities[i], i);
                     answer += 5;
                 }
             }
-        } else {
-            answer += cities.length * 5;
         }
         
         return answer;
+    }
+    
+    public void cacheMiss(Map<String, Integer> cache, String city, int idx){
+        String target = "";
+        int min = 1000000;
+        for(String s : cache.keySet()){
+            if(min > cache.get(s)){
+                target = s;
+                min = cache.get(s);
+            }
+        }
+        
+        cache.remove(target);
+        cache.put(city, idx);
     }
 }
