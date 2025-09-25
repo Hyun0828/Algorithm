4a2d181980ff8dd863b4f50f273558b0fafd1e83
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-25 17:54:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.41 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,43 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        bfs(maps, n, m);
-        
-        if(maps[n-1][m-1] == 1)
-            return -1;
-        return maps[n-1][m-1];
+        int h = maps.length;
+        int w = maps[0].length;
+        return bfs(w, h, maps);
     }
     
-    public void bfs(int[][] maps, int n, int m){
+    public int bfs(int w, int h, int[][] maps) {
         Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
-        int[] dx = {-1, 1, 0 ,0};
+        boolean[][] visited = new boolean[h][w];
+        int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
         
-        queue.add(new int[]{0, 0});
+        queue.add(new int[]{0,0});
+        // visited[0][0] = true;
         
         while(!queue.isEmpty()){
-            int[] current = queue.poll();
-            int y = current[0];
-            int x = current[1];
+            int[] cur = queue.poll();
+            int cy = cur[0];
+            int cx = cur[1];
             
-            if(visited[y][x])
+            if(cy == h-1 && cx == w-1)
+                break;
+            
+            if(visited[cy][cx])
                 continue;
-            visited[y][x] = true;
+            visited[cy][cx] = true;
             
             for(int i=0; i<4; i++){
-                int ny = y + dy[i];
-                int nx = x + dx[i];
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
                 
-                if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || maps[ny][nx] == 0)
+                if(ny < 0 || ny >= h || nx < 0 || nx >= w || visited[ny][nx] || maps[ny][nx] == 0)
                     continue;
                 
-                maps[ny][nx] = maps[y][x] + 1;
+                maps[ny][nx] = maps[cy][cx] + 1;
                 queue.add(new int[]{ny, nx});
             }
         }
+        
+        if(maps[h-1][w-1] == 1)
+            return -1;
+        return maps[h-1][w-1];
     }
 }
