7da398b017314a8a3f09f32785b32231a9a407d0
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-25 19:12:56 +0900

    [level 2] Title: 타겟 넘버, Time: 258.11 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -9,34 +9,25 @@ class Solution {
         return answer;
     }
     
-    public void bfs(int[] numbers, int target){
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(-1, 0));
+    public void bfs(int[] numbers, int target) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
         
         while(!queue.isEmpty()){
-            Node node = queue.poll();
-            int idx = node.height;
-            int value = node.value;
+            int[] cur = queue.poll();
+            int height = cur[0];
+            int value = cur[1];
             
-            if(value == target && idx == numbers.length - 1){
+            if(value == target && height == numbers.length){
                 answer++;
             }
             
-            if(idx + 1 < numbers.length){
-                queue.add(new Node(idx+1, value+numbers[idx+1]));
-                queue.add(new Node(idx+1, value-numbers[idx+1]));
-            }
-
-        }
-    }
-    
-    public static class Node {
-        int height;
-        int value;
-        
-        public Node(int height, int value){
-            this.height = height;
-            this.value = value;
+            if(height >= numbers.length)
+                continue;
+            
+            queue.add(new int[]{height + 1, value + numbers[height]});
+            queue.add(new int[]{height + 1, value - numbers[height]});
+            height++;
         }
     }
 }
