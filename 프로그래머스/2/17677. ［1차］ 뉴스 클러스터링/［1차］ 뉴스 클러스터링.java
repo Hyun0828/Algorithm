45a5a518728764b4e265dd982de6812073c986bd
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-26 14:48:57 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 4.32 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -2,86 +2,87 @@ import java.util.*;
 
 class Solution {
     public int solution(String str1, String str2) {
+        int answer = 0;
         
-        str1 = this.toLowerCase(str1);
-        str2 = this.toLowerCase(str2);
+        Map<String, Integer> map1 = convert(str1);
+        Map<String, Integer> map2 = convert(str2);
         
-        Map<String, Integer> map1 = this.splitString(str1);
-        Map<String, Integer> map2 = this.splitString(str2);    
+        // 교집합 & 합집합 구하기
+        int guo = guo(map1, map2);
+        int hap = hap(map1, map2);
         
-        return this.getSimilarity(map1, map2);
+        if(guo == 0 && hap == 0)
+            return 65536;
+        return (int)Math.floor((double)guo / hap * 65536);
     }
     
-    public String toLowerCase(String str){
-        return str.toLowerCase();
+    public int guo(Map<String, Integer> map1, Map<String, Integer> map2){
+        Map<String, Integer> map = new HashMap<>();
+        for(String s : map1.keySet()){
+            if(map2.containsKey(s)){
+                map.put(s, Math.min(map1.get(s), map2.get(s)));
+            }
+        }
+        
+        for(String s : map2.keySet()){
+            if(map1.containsKey(s)){
+                map.put(s, Math.min(map1.get(s), map2.get(s)));
+            }
+        }
+        
+        int answer = 0;
+        for(String s : map.keySet()){
+            answer += map.get(s);
+        }
+        
+        return answer;
     }
     
-    public Map<String, Integer> splitString(String str){
+    public int hap(Map<String, Integer> map1, Map<String, Integer> map2){
         Map<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<str.length() - 1; i++){
-            char a = str.charAt(i);
-            char b = str.charAt(i+1);
-            
-            if(Character.isLetter(a) && Character.isLetter(b)){
-                StringBuilder sb = new StringBuilder();
-                sb.append(a);
-                sb.append(b);
-                String s = sb.toString();
-                
-                map.put(s, map.getOrDefault(s, 0) + 1);
+        for(String s : map1.keySet()){
+            if(map2.containsKey(s)){
+                map.put(s, Math.max(map1.get(s), map2.get(s)));
+            } else {
+                map.put(s, map1.get(s));
             }
+        }
+                        
+        for(String s : map2.keySet()){
+            if(map1.containsKey(s)){
+                map.put(s, Math.max(map1.get(s), map2.get(s)));
+            } else {
+                map.put(s, map2.get(s));
+            }
+        }
+        
+        int answer = 0;
+        for(String s : map.keySet()){
+            answer += map.get(s);
+        }
+        
+        return answer;
+    }
+    
+    public Map<String, Integer> convert(String s){
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<s.length() - 1; i++){
+            String sub = s.substring(i, i+2);
+            boolean isLetter = true;
+            for(int j=0; j<2; j++){
+                if(!Character.isLetter(sub.charAt(j))){
+                    isLetter = false;
+                    break;
+                }
+            }
+            if(!isLetter)
+                continue;
+            sub = sub.toUpperCase();
+            map.put(sub, map.getOrDefault(sub, 0) + 1);
         }
         
         return map;
-    }
-    
-    // 합집합과 교집합을 구한다.
-    public int getSimilarity(Map<String, Integer> map1, Map<String, Integer> map2){
-        
-        if(map1.isEmpty() && map2.isEmpty())
-            return 65536;
-        
-        Map<String, Integer> intersect = new HashMap<>();
-        Map<String, Integer> union = new HashMap<>();
-        Set<String> both = new HashSet<>();
-        
-        int num_intersect = 0;
-        int num_union = 0;
-        
-        // 중복 문자열을 먼저 찾는다.
-        for(String key : map1.keySet()){
-            if(map2.containsKey(key)){
-                int min = Math.min(map1.get(key), map2.get(key));
-                int max = Math.max(map1.get(key), map2.get(key));
-                
-                intersect.put(key, min);
-                union.put(key, max);
-                both.add(key);
-            }
-        }
-        
-        for(String key : map1.keySet()){
-            if(!both.contains(key)){
-                union.put(key, map1.get(key));
-            }
-        }
-        
-        for(String key : map2.keySet()){
-            if(!both.contains(key)){
-                union.put(key, map2.get(key));
-            }
-        }
-        
-        // 유사도를 계산한다.
-        for(String key : intersect.keySet()){
-            num_intersect += intersect.get(key);
-        }
-        
-        for(String key : union.keySet()){
-            num_union += union.get(key);
-        }
-        
-        return (int)65536 * num_intersect / num_union;
     }
 }
