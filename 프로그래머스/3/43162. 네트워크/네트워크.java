383d82f1811fb418294a5a95e0697c2741570197
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-28 13:39:33 +0900

    [level 3] Title: 네트워크, Time: 0.35 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -7,39 +7,52 @@ class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
+        
+        // 1 1 0
+        // 1 1 0
+        // 0 0 1
+        
+        // 연결되었는지 확인 -> 그래프 탐색 -> dfs/bfs
+        // 그래프를 저장하는 자료구조 -> 행렬/연결리스트
+        // 탐색 안 한 노드를 찾아서 탐색 하면서 탐색 횟수 = 네트워크 개수
+        
         while(true){
-            int idx = isComplete(n);
-            if(idx == -1)
+            int start = findStart(n);
+            if(start != -1){
+                bfs(computers, n, start);
+                answer++;
+            } else {
                 break;
-            bfs(idx, n, computers);
-            answer++;
+            }
         }
+        
         return answer;
     }
     
-    public int isComplete(int n){
+    public int findStart(int n){
         for(int i=0; i<n; i++){
             if(!visited[i])
-                return i; 
+                return i;
         }
         return -1;
     }
     
-    public void bfs(int start, int n, int[][] computers){
+    public void bfs(int[][] computers, int n, int start){
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
+        visited[start] = true;
         
         while(!queue.isEmpty()){
-            int current = queue.poll();
-            
-            if(visited[current])
-                continue;
-            visited[current] = true;
+            int cur = queue.poll();
             
             for(int i=0; i<n; i++){
-                if(i!=n && computers[current][i] == 1){
-                    if(!visited[i])
-                        queue.add(i);
+                if(i==cur)
+                    continue;
+                
+                int computer = computers[cur][i];
+                if(computer == 1 && !visited[i]){
+                    visited[i] = true;
+                    queue.add(i);
                 }
             }
         }
