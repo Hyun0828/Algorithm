7bee720e3b4cead330276fe65448a8e7ed6ae89b
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-28 13:58:13 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 200.97 ms, Memory: 204 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -4,38 +4,27 @@ class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
         
-        Stack<Pair> s = new Stack<>();
+        // 1. 길이가 100만이니까 완탐은 시간초과
+        // 2. 정렬 시킬 수 없으니까 이분탐색 불가능
+        // 3. DP? 뒤에 있는 숫자 정보가 필요하니까 뒤에서 부터 시작하긴 해야 한다. 아닌거같긴함
+        // 4. stack의 냄새가 나긴 하는데, 
+        Stack<Integer> stack = new Stack<>();
         for(int i=numbers.length-1; i>=0; i--){
-            
-            while(!s.isEmpty()){
-                Pair p = s.peek();
-                
-                if(numbers[i] < p.value){
-                    answer[i] = p.value;
-                    s.push(new Pair(numbers[i], p.idx));
-                    break;
-                } else {
-                    s.pop();
-                }
-            }
-            
-            if(s.isEmpty()){
-                s.push(new Pair(numbers[i], i));
+            if(stack.isEmpty()){
                 answer[i] = -1;
+                stack.push(numbers[i]);
+            } else {
+                while(!stack.isEmpty() && numbers[i] >= stack.peek()){
+                    stack.pop();
+                }
+                if(stack.isEmpty())
+                    answer[i] = -1;
+                else 
+                    answer[i] = stack.peek();
+                stack.push(numbers[i]);
             }
         }
-            
         
         return answer;
-    }
-    
-    static class Pair {
-        int value;
-        int idx;
-        
-        public Pair(int value, int idx){
-            this.value = value;
-            this.idx = idx;
-        }
     }
 }
