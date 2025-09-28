b951e59072a15030d2ae5d57a5ccff8ee39bc003
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-28 13:46:05 +0900

    [level 3] Title: 정수 삼각형, Time: 10.57 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,28 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] triangle) {        
-        int n = triangle.length;
-        int m = triangle[triangle.length-1].length;
-        int[][] dp = new int[n][m];
-        dp[0][0] = triangle[0][0];
+    public int solution(int[][] triangle) {
+        int answer = 0;
         
-        for(int i=1; i<n; i++){
+        // 완탐은 2^500이니까 당연히 안 되겠지
+        // DP가 가능할까? save and reuse의 관점에서 바라보면, 아래로 내려올 때는 위에서 최댓값에 현재 값을 더해주면 된다.
+        
+        // 7
+        // 3 8
+        // 8 1 0
+        // 2 7 4 4
+        // 4 5 2 6 5
+        
+        for(int i=1; i<triangle.length; i++){
             for(int j=0; j<triangle[i].length; j++){
-                if(j==0)
-                    dp[i][j] = dp[i-1][j] + triangle[i][j];
-                else if(j==triangle[i].length - 1)
-                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
-                else 
-                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                if(j==0){
+                    triangle[i][j] = triangle[i-1][j] + triangle[i][j];
+                } else if(j==triangle[i].length-1){
+                    triangle[i][j] = triangle[i-1][j-1] + triangle[i][j];
+                } else {
+                    triangle[i][j] = Math.max(triangle[i-1][j-1], triangle[i-1][j]) + triangle[i][j];
+                }
             }
         }
         
-        int answer = 0;
-        for(int i=0; i<m; i++){
-            if(answer < dp[n-1][i])
-                answer = dp[n-1][i];
+        for(int i=0; i<triangle[triangle.length-1].length; i++){
+            answer = Math.max(triangle[triangle.length-1][i], answer);
         }
+        
         return answer;
     }
 }
