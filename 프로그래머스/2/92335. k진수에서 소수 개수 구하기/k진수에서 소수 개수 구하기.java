751704558b895ebe2cd5be9e5f07a61277a89e51
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-28 18:54:12 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 25.94 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,18 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        String s = Integer.toString(n, k);
         
-        for (int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
-                String sub = s.substring(i, j+1);
-                long num = Long.parseLong(sub);
-                if(num == 1)
-                    continue;
+        String s = Integer.toString(n, k);
+        for(int i=0; i<s.length(); i++){
+            for(int j=i+1; j<=s.length(); j++){
+                String sub = s.substring(i, j);
                 
-                if(isPrime(num) && hasZero(sub) && isOk(i, j, s)){
-                    System.out.println(num);
-                    answer++;
+                if(isPrime(Long.parseLong(sub)) && !hasZero(sub)){
+                    System.out.println(i + ", " + j + ", " + sub);
+                    if(i==0 && j==s.length()){
+                        System.out.println("P : " + sub);
+                        answer++;
+                    } else if(i > 0 && j < s.length() && s.charAt(i-1) == '0' && s.charAt(j) == '0'){
+                        System.out.println("0P0 : " + sub);
+                        answer++;
+                    } else if(i == 0 && j < s.length() && s.charAt(j) == '0'){
+                        System.out.println("P0 : " + sub);
+                        answer++;
+                    } else if(i > 0 && j == s.length() && s.charAt(i-1) == '0'){
+                        System.out.println("0P : " + sub);
+                        answer++;
+                    } 
                 }
             }
         }
@@ -22,43 +31,22 @@ class Solution {
         return answer;
     }
     
-    public static boolean isPrime(long n){
-        for(int i=2; i<=Math.sqrt(n); i++){
-            if(n%i == 0)
+    public boolean isPrime(long num){
+        if(num <= 1)
+            return false;
+        
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num % i == 0)
                 return false;
         }
         return true;
     }
     
-    public static boolean hasZero(String s){
+    public boolean hasZero(String s){
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) == '0')
-                return false;
-        }
-        return true;
-    }
-    
-    public static boolean isOk(int start, int end, String s){
-
-        if(start == 0 && end == s.length()-1){
-            return true;
-        }
-        
-        if(start == 0 && end < s.length() - 1){
-            if(s.charAt(end+1) == '0')
                 return true;
         }
-        
-        if(start > 0 && end == s.length() -1) {
-            if(s.charAt(start-1) == '0')
-                return true;
-        }
-        
-        if(start > 0 && end < s.length() - 1){
-            if(s.charAt(start-1) == '0' && s.charAt(end+1) == '0')
-                return true;
-        }
-        
         return false;
     }
 }
