a32dc1142853bfc559ffbab687cf234d16105ac6
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-29 10:38:59 +0900

    [level 3] Title: 이중우선순위큐, Time: 72.65 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,48 +1,44 @@
-import java.util.*;
+import java.util.*; 
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        
-        // 우선순위 큐 : 최대힙/최소힙
-        // 큐가 1개면 최댓값 연산 or 최솟값 연산 둘 중 1개만 처리할 수 있다.
-        // 그럼 큐를 2개 쓰면 처리할 수 있을까?
-        // 최댓값 연산은 최대힙에서, 최솟값 연산은 최소힙에서 처리한다면,, 추가 연산은?
-        // 일단 이건 안 되긴 함. 그냥 해보면 답이 이상함
-        
-        // 삽입하는 숫자들을 Map<Integer, Integer>로 관리하고
-        // 각 힙에서 삭제할 때 map에서 개수를 줄인다.
-        
-        // -45, 653, 45, 97, 333
-        // -45, 45, 333
-        
-        // -45 45 333  
+        int[] answer = {};
         
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         Map<Integer, Integer> map = new HashMap<>();
         
         for(int i=0; i<operations.length; i++){
-            String op = operations[i].split(" ")[0];
-            int num = Integer.parseInt(operations[i].split(" ")[1]);
-            
-            if(op.equals("I")){
+            char op = operations[i].charAt(0);
+            String s = operations[i].substring(2);
+            int num = Integer.parseInt(s);
+            if(op == 'I'){
                 maxHeap.add(num);
                 minHeap.add(num);
                 map.put(num, map.getOrDefault(num, 0) + 1);
-            } else if(op.equals("D")){
-                if(num == 1){
-                    while(!maxHeap.isEmpty()){
-                        int n = maxHeap.poll();
-                        if(map.get(n) > 0){
+            } else if(op == 'D'){
+                if(num == -1){
+                    if(minHeap.isEmpty())
+                        continue;
+                    while(!minHeap.isEmpty()){
+                        int n = minHeap.peek();
+                        if(map.get(n) == 0)
+                            minHeap.poll();
+                        else {
+                            minHeap.poll();
                             map.put(n, map.get(n) - 1);
                             break;
                         }
                     }
-                } else {
-                    while(!minHeap.isEmpty()){
-                        int n = minHeap.poll();
-                        if(map.get(n) > 0){
+                } else if (num == 1){
+                    if(maxHeap.isEmpty())
+                        continue;
+                    while(!maxHeap.isEmpty()){
+                        int n = maxHeap.peek();
+                        if(map.get(n) == 0)
+                            maxHeap.poll();
+                        else {
+                            maxHeap.poll();
                             map.put(n, map.get(n) - 1);
                             break;
                         }
@@ -51,22 +47,27 @@ class Solution {
             }
         }
         
+        // 
+        // 123
+        
         while(!maxHeap.isEmpty()){
-            int n = maxHeap.poll();
-            if(map.get(n) > 0){
-                answer[0] = n;
+            int n = maxHeap.peek();
+            if(map.get(n) == 0)
+                maxHeap.poll();
+            else
                 break;
-            }
         }
+        
         while(!minHeap.isEmpty()){
-            int n = minHeap.poll();
-            if(map.get(n) > 0){
-                answer[1] = n;
+            int n = minHeap.peek();
+            if(map.get(n) == 0)
+                minHeap.poll();
+            else
                 break;
-            }
         }
         
-        
-        return answer;
+        if(maxHeap.isEmpty() && minHeap.isEmpty())
+            return new int[]{0, 0};        
+        return new int[]{maxHeap.poll(), minHeap.poll()};
     }
 }
