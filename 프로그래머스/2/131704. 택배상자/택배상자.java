041374fa293b975c322c68b5d8898f80adc3bbc3
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-30 17:06:05 +0900

    [level 2] Title: 택배상자, Time: 145.88 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,41 +1,46 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] order) {
-        int answer = 0;
-        int n = order.length;
-        int mainBelt = 1;
-        Stack<Integer> subBelt = new Stack<>();
+    public int solution(int[] order) {        
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+        int currentBox = 1;
+        int idx = 0;
         
-        for(int i=0; i<n; i++) {
-            int box = order[i];
-            
-            // 메인 벨트에서 꺼낼 수 있으면
-            if(mainBelt == box) {
-                mainBelt++;
-                answer++;
-                continue;
-            }
-            
-            // 보조 벨트에서 꺼낼 수 있으면
-            if(!subBelt.isEmpty() && subBelt.peek() == box) {
-                subBelt.pop();
-                answer++;
-                continue;
-            }
-            
-            // 컨테이너 벨트의 맨 앞에 놓인 상자가 현재 트럭에 실어야 하는 순서가 아니라면 보조 컨테이너 벨트에 둔다.
-            if(mainBelt != box) {
-                // 꺼낼 수가 없으면 끝
-                if(!subBelt.isEmpty() && subBelt.peek() > box){
+        // 5 4 3 2 1
+        // 1 2 3 4 5
+        
+        while(true){
+            if(currentBox > order.length || currentBox != order[idx]){
+                boolean flag = true;
+                // 보조컨테이너에서 꺼낼 수 있으면
+                while(!stack.isEmpty()){
+                    if(stack.peek() == order[idx]){
+                        count++;
+                        stack.pop();
+                        idx++;
+                    } else {
+                        flag = false;
+                        break;
+                    }
+                }
+                
+                if(!flag && currentBox == order.length && currentBox != order[idx]){
                     break;
                 }
-                subBelt.push(mainBelt++);
-                i--;
-                continue;
+                
+                if(idx == order.length)
+                    break;
+                
+                stack.push(currentBox++);
+            } else {
+                count++;
+                currentBox++;
+                if(idx < order.length - 1)
+                    idx++;
             }
         }
         
-        return answer;
+        return count;
     }
 }
