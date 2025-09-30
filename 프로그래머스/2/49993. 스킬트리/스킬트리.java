f048d71372424ae44f4c8cba65726fb58a4db566
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-09-30 17:15:56 +0900

    [level 2] Title: 스킬트리, Time: 0.19 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -5,52 +5,29 @@ class Solution {
         int answer = 0;
         
         for(int i=0; i<skill_trees.length; i++){
-            Map<Character, Integer> map = getMap(skill_trees[i]);
-            int num = -1;
-            boolean isOk = true;
-            boolean flag = false;
-
-            for(int j=0; j<skill.length(); j++){
-                Character c = skill.charAt(j);
-                
-                if(flag && map.containsKey(c)){
-                    isOk = false;
-                    break;
-                }
-                
-                if(map.containsKey(c)) {
-                    if(num > map.get(c)) {
-                        isOk = false;
-                        break;
-                    } else {
-                        num = map.get(c);
-                    }
-                } else {
-                    if(j == skill.length() - 1) {
-                        // 문제 없음
-                    } else {
-                        // 뒤에 나오는 모든 문자에 대해 값이 없어야 Ok
-                        flag = true;
-                    }
-                } 
-            }
-            
-            if(isOk){
-                System.out.println(skill_trees[i]);
+            if(isOk(skill, skill_trees[i]))
                 answer++;
-            }
         }
         
         return answer;
     }
     
-    public static Map<Character, Integer> getMap(String skill_tree){
-        Map<Character, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<skill_tree.length(); i++){
-            map.put(skill_tree.charAt(i), i);
+    public boolean isOk(String skill, String skill_tree){
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<skill.length(); i++){
+            char c = skill.charAt(i);
+            int index = skill_tree.indexOf(c);
+            if(index == -1)
+                list.add(100);
+            else
+                list.add(index);
         }
-        
-        return map;
+        int idx = list.get(0);
+        for(int i=1; i<list.size(); i++){
+            if(idx > list.get(i))
+                return false;
+            idx = list.get(i);
+        }
+        return true;
     }
 }
