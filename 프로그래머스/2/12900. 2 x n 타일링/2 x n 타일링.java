923bf706c507ed33e8ffa091eead35690741986f
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-01 17:24:16 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.18 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int m = 1000000007;
+        int N = 1000000007;
+        
         int[] dp = new int[n+1];
-        dp[0] = 1;
+        dp[0] = 0;
         dp[1] = 1;
-        for(int i=2; i<=n; i++){
-            dp[i] = ((dp[i-1] % m) + (dp[i-2] % m))%m;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = ((dp[i-1]%N) + (dp[i-2]%N))%N;
         }
         
         return dp[n];
