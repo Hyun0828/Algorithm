6c2bc7db450d69f0bfda78b45a56b3b063b9beba
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-05 09:40:01 +0900

    [level 3] Title: 단속카메라, Time: 9.20 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 58.2 MB, 시간: 13.11 ms
+메모리: 58 MB, 시간: 9.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 19일 21:12:59
+2025년 10월 05일 09:39:59
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 1;
+        int answer = 0;
+        Arrays.sort(routes, (a,b) -> a[1] - b[1]);
         
-        Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1]));
-        int cam = routes[0][1];
+        int idx = routes[0][1];
+        answer++;
         
-        for(int i=1; i<routes.length; i++){
-            if(cam < routes[i][0]){
+        for(int i=1; i<routes.length; i++) {
+            if(idx < routes[i][0]) {
+                idx = routes[i][1];
                 answer++;
-                cam = routes[i][1];
             }
         }
         
