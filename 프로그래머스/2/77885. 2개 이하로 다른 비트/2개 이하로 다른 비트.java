f6984fc5e1c572107a95c7bc0723c1d23b82812f
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-07 10:06:19 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 112.24 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -3,40 +3,66 @@ import java.util.*;
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-    
+        
         for(int i=0; i<numbers.length; i++){
-            long number = numbers[i];
-            if(number % 2 == 0){
-                answer[i] = number + 1;
-                continue;
-            }
-            
-            char[] arr = Long.toString(number, 2).toCharArray();
-            boolean flag = false;
-            for(int j=arr.length-1; j>=0; j--){
-                if(arr[j] == '0'){
-                    arr[j] = '1';
-                    arr[j+1] = '0';
-                    flag = true;
-                    break;
-                }
-            }
-            
-            char[] temp = new char[arr.length+1];
-            if(!flag){
-                temp[0] = '1';
-                temp[1] = '0';
-                for(int j=2; j<temp.length; j++){
-                    temp[j] = arr[j - 1];
+            if(numbers[i] % 2 == 0)
+                answer[i] = numbers[i] + 1;
+            else {
+                StringBuilder sb = new StringBuilder();
+                String s = Long.toString(numbers[i], 2);
+                int idx = -1;
+                for(int j=s.length()-1; j>=0; j--){
+                    if(s.charAt(j) == '0'){
+                        idx = j;
+                        break;
+                    }
                 }
                 
-                String s = new String(temp);
-                answer[i] = Long.parseLong(s, 2);
-            } else {
-                String s = new String(arr);
-                answer[i] = Long.parseLong(s, 2);
+                if(idx == -1){
+                    sb.append('1');
+                    sb.append('0');
+                    for(int j=1; j<s.length(); j++){
+                        sb.append(s.charAt(j));
+                    }
+                } else {
+                    for(int j=0; j<s.length(); j++){
+                        if(idx != j){
+                            sb.append(s.charAt(j));
+                        } else {
+                            sb.append('1');
+                            sb.append('0');
+                            j++;
+                        }
+                    }
+                }
+                
+                answer[i] = Long.parseLong(sb.toString(), 2);
             }
         }
+        
+        // n이 짝수면 n+1
+        // n이 홀수면,
+        
+        // 2 : 0010
+        // 3 : 0011
+        // 4 : 0100
+        // 5 : 0101
+        // 6 : 0110
+        // 7 : 0111
+        // 8 : 1000
+        // 9 : 1001
+        // 10 : 1010
+        // 11 : 1011
+        // 12 : 1100
+        // 13 : 1101
+        // 14 : 1110
+        // 15 : 1111
+        // 16 : 10000
+        // 10001
+        // 10010
+        // 10011
+        // 10100
+        // 10101
         
         return answer;
     }
