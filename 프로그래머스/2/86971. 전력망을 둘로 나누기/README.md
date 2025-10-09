683482b3d17c975b660e57adf0bc01ecc331ac16
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-09 11:15:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.78 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971#qna) 
 
 ### 성능 요약
 
-메모리: 85.8 MB, 시간: 6.93 ms
+메모리: 81.5 MB, 시간: 2.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 09일 11:01:16
+2025년 10월 09일 11:15:44
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,73 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    int[] parent;
-    int[] rank;
+    List<Integer>[] graph;
+    boolean[] visited;
+    int cnt = 0;
     
     public int solution(int n, int[][] wires) {
         int answer = 1000000000;
         
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        
+        for(int i=1; i<=n; i++)
+            graph[i] = new ArrayList<>();
+        
         for(int i=0; i<wires.length; i++){
-            init(n);
-            for(int j=0; j<wires.length; j++){
-                if(i == j)
-                    continue;
-                int a = wires[j][0];
-                int b = wires[j][1];
-                union(a, b);
-            }
+            int a = wires[i][0];
+            int b = wires[i][1];
             
-            Map<Integer, Integer> map = new HashMap<>();
-            Set<Integer> set = new HashSet<>();
-            for(int z=1; z<=n; z++){
-                int root = find(z);
-                set.add(root);
-                map.put(root, map.getOrDefault(root, 0) + 1);
-            }
-            if(map.size() < 2)
-                continue;
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        
+        for(int i=0; i<wires.length; i++){
+            int a = wires[i][0];
+            int b = wires[i][1];
             
-            int[] tmp = new int[2];
-            int idx = 0;
-            for(Integer k : set){
-                tmp[idx++] = map.get(k);
-            }
+            graph[a].remove(Integer.valueOf(b));
+            graph[b].remove(Integer.valueOf(a));
             
-            answer = Math.min(answer, Math.abs(tmp[0] - tmp[1]));
+            dfs(1);
+            
+            answer = Math.min(answer, Math.abs(cnt - (n - cnt)));
+            
+            cnt = 0;
+            graph[a].add(b);
+            graph[b].add(a);
+            Arrays.fill(visited, false);
         }
         
         return answer;
     }
     
-    public void init(int n){
-        parent = new int[n+1];
-        rank = new int[n+1];
-        
-        for(int i=1; i<=n; i++){
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
-    
-    public int find(int x){
-        if(x == parent[x])
-            return parent[x];
-        return parent[x] = find(parent[x]);
-    }
-    
-    public void union(int a, int b){
-        int pa = find(a);
-        int pb = find(b);
-        
-        if(pa == pb)
-            return;
-        
-        if(rank[pa] > rank[pb]){
-            parent[pb] = parent[pa];
-        } else {
-            parent[pa] = parent[pb];
-            if(rank[pa] == rank[pb])
-                rank[pb]++;
+    public void dfs(int n){
+        visited[n] = true;
+        cnt++;
+        for(Integer i : graph[n]){
+            if(visited[i])
+                continue;
+            dfs(i);
         }
     }
 }
