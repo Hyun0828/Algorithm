ca8bca3ab0c3b92b4acba4eab560dc724e49a5ce
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-10 09:45:23 +0900

    [level 2] Title: 호텔 대실, Time: 10.14 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -3,34 +3,53 @@ import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        Book[] books = new Book[book_time.length];
-        for(int i=0; i<book_time.length; i++){
-            books[i] = new Book(convert(book_time[i][0]), convert(book_time[i][1]));
-        }
-        Arrays.sort(books, (a,b) -> a.start - b.start);
         
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(Book book : books){
-            if(!pq.isEmpty() && pq.peek() <= book.start)
-                pq.poll();
-            pq.offer(book.end + 10);
+        List<Room> list = new ArrayList<>();
+        
+        for(int i=0; i<book_time.length; i++){
+            String[] b = book_time[i];
+            int enter = convert(b[0]);
+            int leave = convert(b[1]);
+            list.add(new Room(enter, leave));
+        }
+        Collections.sort(list);
+        
+        answer++;
+        pq.add(list.get(0).leave);
+        for(int i=1; i<list.size(); i++){
+            Room room = list.get(i);
+            
+            if(!pq.isEmpty()){
+                if(pq.peek()+10 <= room.enter){
+                    pq.poll();
+                } else {
+                    answer++;
+                }
+                pq.add(room.leave);
+            }
         }
         
-        return pq.size();
+        return answer;
     }
     
-    public static int convert(String time){
-        String[] s = time.split(":");
-        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
+    public int convert(String time){
+        String[] t = time.split(":");
+        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
     }
     
-    public static class Book {
-        int start;
-        int end;
+    public static class Room implements Comparable<Room> {
+        int enter;
+        int leave;
         
-        public Book(int s, int e){
-            start = s;
-            end = e;
+        public Room(int enter, int leave){
+            this.enter = enter;
+            this.leave = leave;
+        }
+        
+        @Override
+        public int compareTo(Room r){
+            return this.enter - r.enter;
         }
     }
 }
