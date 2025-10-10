c5bcdf9dc9055586cc80dc1d62345c474011c440
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-10 23:11:03 +0900

    [level 2] Title: 배달, Time: 3.46 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,73 +2,76 @@ import java.util.*;
 
 class Solution {
     
-    static int[][] graph;
-    static int[] distance;
-    static boolean[] visited;
-    static int INF = Integer.MAX_VALUE;
+    int[][] graph;
+    int INF = Integer.MAX_VALUE;
+    int[] distance;
     
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        
+
         graph = new int[N+1][N+1];
-        distance = new int[N+1];
-        Arrays.fill(distance, INF);
-        visited = new boolean[N+1];
-        
         for(int i=1; i<=N; i++){
             Arrays.fill(graph[i], INF);
         }
-
+        distance = new int[N+1];
+        Arrays.fill(distance, INF);
+        
         for(int i=0; i<road.length; i++){
             int from = road[i][0];
             int to = road[i][1];
             int weight = road[i][2];
-        
+            
             graph[from][to] = Math.min(graph[from][to], weight);
             graph[to][from] = Math.min(graph[to][from], weight);
         }
         
-        Dijkstra(N);
+        dijkstra(N);
         
         for(int i=1; i<=N; i++){
             if(distance[i] <= K)
                 answer++;
         }
-        
-        
+
         return answer;
     }
     
-    public static void Dijkstra(int N){
+    public void dijkstra(int N) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[N+1];
         distance[1] = 0;
         
-        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
-        pq.offer(new Edge(1, 0));
+        pq.add(new Edge(1, 0));
         
         while(!pq.isEmpty()){
-            Edge current = pq.poll(); // 최단 거리 노드 꺼내기
-            int u = current.to;
+            Edge edge = pq.poll();
+            int u = edge.to;
             
             if(visited[u])
                 continue;
-            visited[u] = true; // 집합 S에 포함시키기
+            visited[u] = true;
             
             for(int i=1; i<=N; i++){
                 if(!visited[i] && graph[u][i] != INF && distance[i] > distance[u] + graph[u][i]){
-                    distance[i] = distance[current.to] + graph[u][i];
-                    pq.offer(new Edge(i, distance[i]));
+                    distance[i] = distance[u] + graph[u][i];
+                    pq.add(new Edge(i, distance[i]));
                 }
-            }            
+            }
         }
     }
     
-    public static class Edge {
+    
+    public static class Edge implements Comparable<Edge>{
         int to;
         int weight;
         
         public Edge(int to, int weight){
             this.to = to;
             this.weight = weight;
+        }
+        
+        @Override
+        public int compareTo(Edge e){
+            return this.weight - e.weight;
         }
     }
 }
