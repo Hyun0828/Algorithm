ef35d0dc342816077be6110be4545fe2ddd5317b
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-12 21:56:18 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 37.80 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,50 +1,53 @@
 import java.util.*;
 
 class Solution {
+    
+    Map<Integer, Map<String, Integer>> map = new HashMap<>();
+    
     public String[] solution(String[] orders, int[] course) {
-        List<String> result = new ArrayList<>();
-        Map<Integer, Map<String, Integer>> map = new HashMap<>();
-        for (int len : course) {
-            map.put(len, new HashMap<>());
-        }
-
-        for (String order : orders) {
+        for(String order : orders){
             char[] arr = order.toCharArray();
             Arrays.sort(arr);
-            for (int len : course) {
-                if (arr.length < len) continue;
-                dfs(arr, len, 0, new StringBuilder(), map.get(len));
+            for(int i=0; i<course.length; i++){
+                combination(arr, "", course[i], 0);
             }
         }
-
-        for (Integer len : map.keySet()) {
-            int max = 2;
-            for (String s : map.get(len).keySet()) {
-                max = Math.max(max, map.get(len).get(s));
+        
+        Set<String> set = new TreeSet<>();
+        for(int i=0; i<course.length; i++){
+            if(!map.containsKey(course[i]))
+                continue;
+            Map<String, Integer> m = map.get(course[i]);
+            int max = 0;
+            for(String s : m.keySet()){
+                max = Math.max(max, m.get(s));
             }
-
-            for (String s : map.get(len).keySet()) {
-                if (map.get(len).get(s) == max) {
-                    result.add(s);
+            for(String s : m.keySet()){
+                if(m.get(s) == max && max > 1){
+                    set.add(s);
                 }
             }
         }
-
-        Collections.sort(result);
-
-        return result.toArray(new String[0]);
+        
+        String[] answer = new String[set.size()];
+        int idx = 0;
+        for(String s : set){
+            answer[idx++] = s;
+        }
+        
+        return answer;
     }
     
-    public static void dfs(char[] arr, int len, int idx, StringBuilder sb, Map<String, Integer> map) {
-        if (sb.length() == len) {
-            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+    public void combination(char[] arr, String s, int count, int start){
+        if(s.length() == count){
+            map.putIfAbsent(s.length(), new HashMap<>());
+            Map<String, Integer> m = map.get(s.length());
+            m.put(s, m.getOrDefault(s, 0) + 1);
             return;
         }
-
-        for (int i = idx; i < arr.length; i++) {
-            sb.append(arr[i]);
-            dfs(arr, len, i + 1, sb, map);
-            sb.deleteCharAt(sb.length() - 1);
+        
+        for(int i=start; i<arr.length; i++){
+            combination(arr, s+arr[i], count, i+1);
         }
     }
 }
