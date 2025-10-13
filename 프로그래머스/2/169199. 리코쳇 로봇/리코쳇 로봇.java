f6fe648e8c07314cf1ccaffbe1c739d518f16705
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-13 15:03:40 +0900

    [level 2] Title: 리코쳇 로봇, Time: 18.97 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,82 +1,86 @@
 import java.util.*;
 
 class Solution {
+    
+    int answer = 0;
+    int N;
+    int M;
+    
     public int solution(String[] board) {
-        int Y = board.length;
-        int X = board[0].length();
+        N = board.length;
+        M = board[0].length();
+        char[][] map = new char[N][M];
+        int sy = 0;
+        int sx = 0;
+        int ey = 0;
+        int ex = 0;
         
-        int[][] map = new int[Y][X];
-        int[][] dis = new int[Y][X];
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        
-        int startX = 0;
-        int startY = 0;
-        int endX = 0;
-        int endY = 0;
-        
-        for(int i=0; i<Y; i++){
-            Arrays.fill(dis[i], 10000000);
-        }
-        
-        for(int i=0; i<board.length; i++){
-            char[] arr = board[i].toCharArray();
-            for(int j=0; j<arr.length; j++){
-                if(arr[j] == 'R'){
-                    startY = i;
-                    startX = j;
-                } else if(arr[j] == 'D'){
-                    map[i][j] = 1;
-                } else if(arr[j] == 'G'){
-                    endY = i;
-                    endX = j;
-                    map[i][j] = 2;
+        for(int i=0; i<N; i++){
+            map[i] = board[i].toCharArray();
+            for(int j=0; j<M; j++){
+                if(map[i][j] == 'R'){
+                    sy = i;
+                    sx = j;
+                } else if(map[i][j] == 'G'){
+                    ey = i;
+                    ex = j;
                 }
             }
         }
         
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(startY, startX, 0));
+        bfs(map, sy, sx, ey, ex);
         
-        while(!queue.isEmpty()){
-            Point point = queue.poll();
-            int currentY = point.y;
-            int currentX = point.x;
-            int currentDis = point.dis;
-            
-            if(map[currentY][currentX] == 2)
-                return dis[currentY][currentX];
-            
-            // 좌 우 상 하
-            for(int i=0; i<4; i++){
-                int nY = currentY;
-                int nX = currentX;
-                
-                while(nY + dy[i] >= 0 && nY + dy[i] < Y && nX + dx[i] >= 0 && nX + dx[i] < X && map[nY+dy[i]][nX+dx[i]] != 1){
-                    nY += dy[i];
-                    nX += dx[i];
-                }
-                
-                if(dis[nY][nX] > currentDis + 1){
-                    dis[nY][nX] = currentDis + 1;
-                    queue.add(new Point(nY, nX, dis[nY][nX]));
-                }
-            }
-        }
-        
-        
-        return -1;
+        return answer > 0 ? answer : -1;
     }
     
-    public static class Point {
-        int y;
-        int x;
-        int dis;
+    public void bfs(char[][] map, int sy, int sx, int ey, int ex){
+        Queue<int[]> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         
-        public Point(int y, int x, int dis){
-            this.y=y;
-            this.x=x;
-            this.dis=dis;
+        queue.add(new int[]{sy, sx, 0});
+        visited.add(sy + "," + sx);
+        
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int cy = cur[0];
+            int cx = cur[1];
+            int cd = cur[2];
+                        
+            if(cy == ey && cx == ex){
+                answer = cd;
+                break;
+            }
+            
+            for(int i=0; i<4; i++){
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
+                int nd = cd + 1;
+                
+                while(true){
+                    if(ny < 0 || nx < 0 || ny >= N || nx >= M)
+                        break;
+                        
+                    if(map[ny][nx] == 'D'){
+                        break;
+                    }
+                    
+                    int ay = ny + dy[i];
+                    int ax = nx + dx[i];
+                    if(ay < 0 || ax < 0 || ay >= N || ax >= M || map[ay][ax] == 'D'){
+                        if(visited.contains(ny + "," + nx)){
+                            break;
+                        }
+                        visited.add(ny + "," + nx);
+                        queue.add(new int[]{ny, nx, nd});
+                        break;
+                    }
+                    
+                    ny += dy[i];
+                    nx += dx[i];
+                }
+            }
         }
     }
 }
