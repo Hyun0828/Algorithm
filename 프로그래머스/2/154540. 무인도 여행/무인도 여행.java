8880811ff3fcc1681ae3f292b7205ffd89bad31e
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-15 10:01:23 +0900

    [level 2] Title: 무인도 여행, Time: 38.39 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,53 +1,78 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int sum = 0;
+    
+    int N;
+    int M;
+    int[][] map;
+    boolean[][] visited;
     
     public int[] solution(String[] maps) {
         List<Integer> result = new ArrayList<>();
-
-        int Y = maps.length;
-        int X = maps[0].length();
-        int[][] map = new int[Y][X];
-
-        for (int i = 0; i < Y; i++) {
-            for (int j = 0; j < X; j++) {
-                if (maps[i].charAt(j) == 'X') {
+        
+        N = maps.length;
+        M = maps[0].length();
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(maps[i].charAt(j) == 'X')
                     map[i][j] = 0;
-                } else {
-                    map[i][j] = Integer.parseInt(maps[i].charAt(j) + "");
-                }
+                else
+                    map[i][j] = maps[i].charAt(j) - '0';
             }
         }
-
-        for (int i = 0; i < Y; i++) {
-            for (int j = 0; j < X; j++) {
-                if (map[i][j] != 0) {
-                    dfs(map, i, j, Y, X);
-                    result.add(sum);
-                    sum = 0;
-                }
+        
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(!visited[i][j] && map[i][j] != 0){
+                    int area = bfs(i, j);
+                    result.add(area);
+                    
+                    System.out.println(i + "," + j + "," + map[i][j] + "," + area);
+                }   
             }
         }
-
-        if(result.isEmpty())
+        
+        if(result.size() == 0)
             return new int[]{-1};
         
+        int[] answer = new int[result.size()];
         Collections.sort(result);
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        for(int i=0; i<answer.length; i++){
+            answer[i] = result.get(i);
+        }
+        
+        return answer;
     }
-    public static void dfs(int[][] map, int y, int x, int Y, int X) {
-        if (y < 0 || y >= Y || x < 0 || x >= X || map[y][x] == 0) {
-            return;
+    
+    public int bfs(int y, int x){
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        queue.add(new int[]{y,x});
+        visited[y][x] = true;
+        
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int cy = cur[0];
+            int cx = cur[1];
+            
+            answer += map[cy][cx];
+            
+            for(int i=0; i<4; i++){
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
+                
+                if(ny < 0 || nx < 0 || ny >= N || nx >= M)
+                    continue;
+                if(map[ny][nx] == 0 || visited[ny][nx])
+                    continue;
+                visited[ny][nx] = true;
+                queue.add(new int[]{ny, nx});
+            }
         }
-
-        sum += map[y][x];
-        map[y][x] = 0;
-
-        for (int i = 0; i < 4; i++) {
-            dfs(map, y + dy[i], x + dx[i], Y, X);
-        }
+        return answer;
     }
 }
