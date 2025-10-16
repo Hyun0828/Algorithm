234f331eb93e99f9a9fcefc5df1f678f681f6a42
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-16 10:39:29 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 13.73 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,79 +1,62 @@
 import java.util.*;
 
 class Solution {
+    
+    int[][] map;
+    
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
         
-        int[][] map = new int[rows][columns];
-        int num = 1;
+        int n = 1;
+        map = new int[rows][columns];
         for(int i=0; i<rows; i++){
             for(int j=0; j<columns; j++){
-                map[i][j] = num++;
+                map[i][j] = n++;
             }
         }
         
-        int idx = 0;
         for(int i=0; i<queries.length; i++){
-            int x1 = queries[i][0] - 1;
-            int y1 = queries[i][1] - 1;
-            int x2 = queries[i][2] - 1;
-            int y2 = queries[i][3] - 1;
+            int y1 = queries[i][0] - 1;
+            int x1 = queries[i][1] - 1;
+            int y2 = queries[i][2] - 1;
+            int x2 = queries[i][3] - 1;
             
-            int min = rotate(x1, y1, x2, y2, map);
-            answer[idx++] = min;
-            // for(int x=0; x<rows; x++){
-            //     for(int y=0; y<columns; y++){
-            //         System.out.print(map[x][y] + ", ");
-            //     }
-            //     System.out.println();
-            // }
+            answer[i] = rotate(y1,x1,y2,x2);
         }
-        
         
         return answer;
     }
     
-    public static int rotate(int x1, int y1, int x2, int y2, int[][] map){
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public int rotate(int y1, int x1, int y2, int x2){
+        int min = 100000000;
+        int temp = map[y1][x1];
         
-        // x1 행
-        int temp1 = map[x1][y2];
-        for(int i=y2; i>y1; i--){
-            map[x1][i] = map[x1][i-1];
+        // 왼쪽 -> 위
+        for (int y = y1; y < y2; y++) {
+            map[y][x1] = map[y + 1][x1];
+            min = Math.min(min, map[y][x1]);
+        }
+
+        // 아래 -> 왼쪽
+        for (int x = x1; x < x2; x++) {
+            map[y2][x] = map[y2][x + 1];
+            min = Math.min(min, map[y2][x]);
+        }
+
+        // 오른쪽 -> 아래
+        for (int y = y2; y > y1; y--) {
+            map[y][x2] = map[y - 1][x2];
+            min = Math.min(min, map[y][x2]);
+        }
+
+        // 위 -> 오른쪽
+        for (int x = x2; x > x1 + 1; x--) {
+            map[y1][x] = map[y1][x - 1];
+            min = Math.min(min, map[y1][x]);
         }
         
-        // y2 열
-        int temp2 = map[x2][y2];
-        for(int i=x2; i>x1; i--){
-            map[i][y2] = map[i-1][y2];
-        }
-        map[x1+1][y2] = temp1;        
-        
-        // x2 행
-        temp1 = map[x2][y1];
-        for(int i=y1; i<y2; i++){
-            map[x2][i] = map[x2][i+1];
-        }
-        map[x2][y2-1] = temp2;
-        
-        // y1 열
-        for(int i=x1; i<x2; i++){
-            map[i][y1] = map[i+1][y1];
-        }
-        map[x2-1][y1] = temp1;
-        
-        for(int i=y1; i<=y2; i++){
-            pq.offer(map[x1][i]);
-        }
-        for(int i=x1; i<=x2; i++){
-            pq.offer(map[i][y2]);
-        }
-        for(int i=y1; i<=y2; i++){
-            pq.offer(map[x2][i]);
-        }
-        for(int i=x1; i<=x2; i++){
-            pq.offer(map[i][y1]);
-        }
-        return pq.peek();
+        map[y1][x1 + 1] = temp;
+
+        return Math.min(min, temp);
     }
 }
