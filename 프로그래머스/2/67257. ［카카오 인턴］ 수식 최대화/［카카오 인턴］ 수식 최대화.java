6fa9649fd46403d0cd82411791020dd98444e266
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-16 11:13:40 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 19.31 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -2,96 +2,72 @@ import java.util.*;
 
 class Solution {
     
+    List<String> ops = new ArrayList<>();
+    Set<Integer> visited = new HashSet<>();
+    char[] arr = new char[]{'+', '-', '*'};
+    
     public long solution(String expression) {
-        List<String> tokens = tokenize(expression);
-        List<String> ops = new ArrayList<>();
-        if(expression.indexOf('+') >= 0)
-            ops.add("+");
-        if(expression.indexOf('-') >= 0)
-            ops.add("-");
-        if(expression.indexOf('*') >= 0)
-            ops.add("*");
-        
-        // 연산자 순서 만들기 (순열 -> DFS)
-        List<List<String>> orders = new ArrayList<>();
-        permute(ops, new boolean[ops.size()], new ArrayList<>(), orders);
-        
         long answer = 0;
-        for(List<String> order : orders){
-            long result = calculate(new ArrayList<>(tokens), order);
+        
+        String[] n = expression.split("[+\\-*]");
+        List<Long> nums = new ArrayList<>();
+        for (String s : n)
+            nums.add(Long.parseLong(s));
+        
+        String[] o = expression.split("[0-9]+");
+        List<String> opList = new ArrayList<>();
+        for (String s : o) {
+            if (!s.equals("")) opList.add(s);
+        }
+        
+        permutation("");
+        for(String op : ops){
+            long result = calculate(nums, opList, op);
             answer = Math.max(answer, Math.abs(result));
         }
         
         return answer;
     }
     
-    private List<String> tokenize(String s){
-        List<String> tokens = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(c == '+' || c == '-' || c == '*') {
-                tokens.add(sb.toString());
-                sb.setLength(0);
-                tokens.add(String.valueOf(c));
-            } else {
-                sb.append(c);
-            }
-        }
-        if(sb.length() > 0)
-            tokens.add(sb.toString());
+    public long calculate(List<Long> numList, List<String> opList, String opOrder){
+        List<Long> nums = new ArrayList<>(numList);
+        List<String> ops = new ArrayList<>(opList);
         
-        return tokens;
-    }
-    
-    private void permute(List<String> ops, boolean[] used, List<String> path, List<List<String>> orders){
-        if(path.size() == ops.size()){
-            orders.add(new ArrayList<>(path));
-            return;
-        }
-        
-        for(int i=0; i<ops.size(); i++){
-            if(used[i])
-                continue;
-            used[i] = true;
-            path.add(ops.get(i));
-            permute(ops, used, path, orders);
-            path.remove(path.size()-1);
-            used[i] = false;
-        }
-    }
-    
-    private long calculate(List<String> tokens, List<String> order){
-        for(int i=0; i<order.size(); i++){
-            String op = order.get(i);
-            
-            for(int j=0; j<tokens.size(); ){
-                String s = tokens.get(j);
-                
-                // 연산자면
-                if(s.equals(op)){
-                    long a = Long.parseLong(tokens.get(j-1));
-                    long b = Long.parseLong(tokens.get(j+1));
-                    long c;
-                    if(s.equals("+")){
-                        c = a + b;
-                    } else if(s.equals("-")){
-                        c = a - b;
-                    } else {
-                        c = a * b;
-                    }
-                    
-                    tokens.set(j-1, String.valueOf(c));
-                    tokens.remove(j);
-                    tokens.remove(j);
+        for(int i=0; i<opOrder.length(); i++){
+            char op = opOrder.charAt(i);
+            for(int j=0; j<ops.size(); j++){
+                if(ops.get(j).charAt(0) == op){
+                    long res = operate(nums.get(j), nums.get(j+1), op);
+                    nums.remove(j+1);
+                    nums.set(j, res);
+                    ops.remove(j);
                     j--;
-                } else {
-                    j++;
                 }
             }
         }
-        return Long.parseLong(tokens.get(0));
+        return nums.get(0);
+    }
+    
+    public long operate(long a, long b, char op){
+        if(op == '+')
+            return a+b;
+        if(op == '-')
+            return a-b;
+        return a*b;
+    }
+    
+    public void permutation(String s){
+        if(s.length() == 3){
+            ops.add(s);
+            return;
+        }
+        
+        for(int i=0; i<arr.length; i++){
+            if(visited.contains(i))
+                continue;
+            visited.add(i);
+            permutation(s+arr[i]);
+            visited.remove(i);
+        }
     }
 }
-
-    
