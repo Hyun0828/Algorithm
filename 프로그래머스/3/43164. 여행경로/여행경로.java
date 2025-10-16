e8571095fd3a5ef0606f4dbba360272f742e3c59
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-16 13:55:33 +0900

    [level 3] Title: 여행경로, Time: 61.97 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,66 +2,64 @@ import java.util.*;
 
 class Solution {
     
-    Map<String, PriorityQueue<String>> tmp = new HashMap<>();
-    Map<String, List<String>> map = new HashMap<>();
+    Map<String, List<String>> airLine = new HashMap<>();
     Map<String, boolean[]> visited = new HashMap<>();
-    List<String> answer = new ArrayList<>();
-        
-    public String[] solution(String[][] tickets) {    
-        init(tickets);
-        dfs("ICN", new Stack<>(), 0, tickets.length);
-        return answer.toArray(new String[0]);
-    }
     
-    public void init(String[][] tickets){
+    List<String> result = new ArrayList<>();
+    
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        
         for(int i=0; i<tickets.length; i++){
-            String from = tickets[i][0];
-            String to = tickets[i][1];
+            String a = tickets[i][0];
+            String b = tickets[i][1];
             
-            tmp.putIfAbsent(from, new PriorityQueue<>());
-            PriorityQueue<String> pq = tmp.get(from);
-            pq.add(to);
+            airLine.putIfAbsent(a, new ArrayList<>());
+            List<String> list = airLine.get(a);
+            list.add(b);
         }
         
-        for(String s : tmp.keySet()){
-            PriorityQueue<String> pq = tmp.get(s);
-            map.putIfAbsent(s, new ArrayList<>());
-            visited.putIfAbsent(s, new boolean[pq.size()]);
-            
-            List<String> list = map.get(s);
-            while(!pq.isEmpty()){
-                list.add(pq.poll());
-            }
+        for(String key : airLine.keySet()){
+            Collections.sort(airLine.get(key));
+            int len = airLine.get(key).size();
+            visited.putIfAbsent(key, new boolean[len]);
         }
+        
+        List<String> t = new ArrayList<>();
+        t.add("ICN");
+        
+        dfs("ICN", t, 0, tickets.length);
+        
+        return result.toArray(new String[0]);
     }
     
-    public boolean dfs(String start, Stack<String> stack, int count, int len) {
-        if(count == len){
-            answer.add("ICN");
-            answer.addAll(stack);
-            return true;
+    public String convert(List<String> route){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<route.size(); i++)
+            sb.append(route.get(i));
+        return sb.toString();
+    }
+    
+    public void dfs(String start, List<String> route, int count, int len){
+        if (count == len) {
+            if (result.isEmpty() || convert(route).compareTo(convert(result)) < 0) {
+                result = new ArrayList<>(route);
+            }
+            return;
         }
         
-        if(!map.containsKey(start)){
-            return false;
-        }
-        
-        List<String> list = map.get(start);
-        boolean[] vis = visited.get(start);
+        List<String> list = airLine.get(start);
+        boolean[] visit = visited.get(start);
+        if (list == null) return;
         
         for(int i=0; i<list.size(); i++){
-            if(vis[i])
+            if(visit[i])
                 continue;
-            
-            vis[i] = true;
-            stack.push(list.get(i));
-            if(dfs(list.get(i), stack, count + 1, len)){
-                return true;
-            }
-            stack.pop();
-            vis[i] = false;
+            visit[i] = true;
+            List<String> nroute = new ArrayList<>(route);
+            nroute.add(list.get(i));
+            dfs(list.get(i), nroute, count + 1, len);
+            visit[i] = false;
         }
-        
-        return false;
     }
 }
