2d12c9f8156f88c75da0810c3c3aa6c418aecfa2
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-17 10:52:07 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.28 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -2,106 +2,91 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-        for(int i=0; i<5; i++){
-            answer[i] = 1;
-        }
+        int[] answer = new int[places.length];
         
         for(int i=0; i<places.length; i++){
             String[] place = places[i];
-            char[][] map = new char[5][5];
-            Set<Point> set = new HashSet<>();
-            
-            for(int j=0; j<place.length; j++){
-                map[j] = place[j].toCharArray();
+            char[][] map = convert(place);
+            List<int[]> points = getStartPoint(map);
+            boolean isGood = true;
+            for(int[] p : points){
+                if(!check(map, p[0], p[1])){
+                    isGood = false;
+                } 
             }
-            
-            // P 위치 찾기
-            for(int j=0; j<5; j++){
-                for(int k=0; k<5; k++){
-                    if(map[j][k] == 'P')
-                        set.add(new Point(j, k));
-                }
-            }
-            
-            // 각 P마다 확인
-            for(Point p : set){
-                int y = p.y;
-                int x = p.x;
-                
-                if(!check(map, y, x)){
-                    answer[i] = 0;
-                    break;
-                }
-            }
-        }    
+            if(isGood)
+                answer[i] = 1;
+            else
+                answer[i] = 0;
+        }
         
         return answer;
     }
     
-    public boolean check(char[][] map, int y, int x){
-        // 좌 우 상 하
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-      
-        // 1. 상하좌우 이웃 : 거리 1 -> 무조건 안 됨
-        for(int i=0; i<4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            
-            if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5)
-                continue;
-            
-            if(map[ny][nx] == 'P')
-                return false;
-        }
-        
-        // 2. 대각선 이웃 : 거리 2 -> 확인해야
-        int[] cx = {-1, -1, 1, 1};
-        int[] cy = {-1, 1, -1, 1};
-        for(int i=0; i<4; i++){
-            int ny = y + cy[i];
-            int nx = x + cx[i];
-            
-            if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5)
-                continue;
-            
-            if(map[ny][nx] == 'P'){
-                if(map[y][nx] != 'X' || map[ny][x] != 'X')
-                    return false;
-            }
-        }
-        
-        // 3. 상하좌우 2 : 거리 2 -> 확인해야
-        for(int i=0; i<4; i++){
-            int ny = y + dy[i] * 2;
-            int nx = x + dx[i] * 2;
-            
-            if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5)
-                continue;
-            
-            if(map[ny][nx] == 'P'){
-                if(i==0 && map[ny][x-1] != 'X')
-                    return false;
-                if(i==1 && map[ny][x+1] != 'X')
-                    return false;
-                if(i==2 && map[y-1][nx] != 'X')
-                    return false;
-                if(i==3 && map[y+1][nx] != 'X')
-                    return false;
-            }
-        }
-        
+    public boolean posCheck(int N, int M, int y, int x){
+        if(y < 0 || x < 0 || y >= N || x >= M)
+            return false;
         return true;
     }
     
-    static class Point {
-        int y;
-        int x;
+    public boolean check(char[][] map, int y, int x){
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        int N = map.length;
+        int M = map[0].length;
         
-        public Point(int y, int x){
-            this.y=y;
-            this.x=x;
+        // 1. 맨해튼 거리가 1일 때는 아예 이웃하면 안 됨
+        for(int i=0; i<4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(!posCheck(N, M, ny, nx))
+                continue;
+            if(map[ny][nx] == 'P'){
+                return false;
+            }
         }
+        
+        // 2. 맨해튼 거리가 2일 때
+        if(posCheck(N, M, y-1, x-1) && (map[y-1][x] != 'X' || map[y][x-1] != 'X') && map[y-1][x-1] == 'P')
+            return false;
+        if(posCheck(N, M, y+1, x-1) && (map[y][x-1] != 'X' || map[y+1][x] != 'X') && map[y+1][x-1] == 'P')
+            return false;
+        if(posCheck(N, M, y-1, x+1) && (map[y][x+1] != 'X' || map[y-1][x] != 'X') && map[y-1][x+1] == 'P')
+            return false;
+        if(posCheck(N, M, y+1, x+1) && (map[y][x+1] != 'X' || map[y+1][x] != 'X') && map[y+1][x+1] == 'P')
+            return false;
+        
+        for(int i=0; i<4; i++){
+            int my = y + dy[i];
+            int mx = x + dx[i];
+            int ny = y + 2 * dy[i];
+            int nx = x + 2 * dx[i];
+            
+            if(!posCheck(N, M, ny, nx))
+                continue;
+            if(map[my][mx] != 'X' && map[ny][nx] == 'P')
+                return false;
+        }
+        return true;
+    }
+    
+    public List<int[]> getStartPoint(char[][] map){
+        List<int[]> list = new ArrayList<>();
+        for(int i=0; i<map.length; i++){
+            for(int j=0; j<map[i].length; j++){
+                if(map[i][j] == 'P'){
+                    list.add(new int[]{i, j});
+                }
+            }
+        }
+        return list;
+    }
+    
+    public char[][] convert(String[] place){
+        char[][] map = new char[place.length][place[0].length()];
+        for(int i=0; i<place.length; i++){
+            map[i] = place[i].toCharArray();
+        }
+        return map;
     }
 }
