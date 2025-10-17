575a5927d3edfc75936a155c762c7961a8e63b2d
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-17 10:16:58 +0900

    [level 2] Title: 괄호 변환, Time: 17.59 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -2,91 +2,76 @@ import java.util.*;
 
 class Solution {
     public String solution(String p) {
-        return solve(p);
+        String w = p;
+        
+        // 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환한다.
+        if(w.equals(""))
+            return "";
+        
+        if(isCorrectString(w))
+            return w;
+        
+        // 2. w를 두 균형잡힌 괄호 문자열 u,v로 분리한다. 단 u는 더 이상 분리할 수 없어야 하며 v는 빈 문자열이 될 수 있다.
+        String[] arr = parseString(w);
+        String u = arr[0];
+        String v = arr[1];
+        System.out.println(u + ", " + v);
+        
+        // 3. 문자열 u가 올바른 괄호 문자열이면 v에 대해 1단계부터 다시 수행하고 그 결과를 u에 이어 붙인 후 반환한다.
+        // 4. 아니라면 4번 과정을 수행한다.
+        if(isCorrectString(u)){
+            return u + solution(v);
+        } else {
+            String s = "(";
+            s += solution(v);
+            s += ")";
+            for(int i=1; i<u.length()-1; i++){
+                if(u.charAt(i) == '(')
+                    s += ")";
+                else
+                    s += "(";
+            }
+            return s;
+        }
     }
     
-    public String solve(String p){
-        // 1. 빈 문자열이면 빈 문자열 반환
-        if(p.isEmpty()){
-            return "";
+    public String[] parseString(String w){
+        String u = "";
+        String v = "";
+        int n1 = 0;
+        int n2 = 0;
+        for(int i=0; i<w.length(); i++){
+            if(w.charAt(i) == '(')
+                n1++;
+            else
+                n2++;
+            
+            if(n1 == n2){
+                u = w.substring(0, i+1);
+                v = w.substring(i+1);
+                break;
+            }
         }
-        // 올바른 괄호 문자열이라면 그대로 반환
-        if(isCorrectString(p)){
-            return p;
-        }
-        
-        // 2. 균형잡힌 괄호 문자열 u, v로 분리
-        String[] uv = splitString(p);
-        String u = uv[0];
-        String v = uv[1];
-        
-        // 3. u가 올바른 괄호 문자열 이라면 v에 대해 1단계부터 수행 후 u에 이어 붙인 후 반환
-        if(isCorrectString(u)){
-            return u + solve(v);
-        }
-        
-        // 4. u가 올바른 괄호 문자열이 아니라면
-        String result = "(";
-        result += solve(v);
-        result += ")";
-        // 4-4. u의 첫, 마지막 문자를 제거하고 나머지 문자열의 괄호 방향을 뒤집어 붙인다.
-        result += reverseString(u);
-        return result;
-        
+        return new String[]{u, v};
     }
     
     public boolean isCorrectString(String s){
         Stack<Character> stack = new Stack<>();
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.push(s.charAt(i));
-            } else {
-                if(stack.isEmpty()){
+            char c = s.charAt(i);
+            
+            if(c == '(')
+                stack.push(c);
+            else {
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                } else {
                     return false;
                 }
-                stack.pop();
             }
-        }
-        return stack.isEmpty();
-    }
-    
-    public String[] splitString(String s){
-        String[] temp = new String[2];
-        
-        if(s.length() == 2){
-            temp[0] = s;
-            temp[1] = "";
-            return temp;
         }
         
-        for(int i=2; i<=s.length(); i+=2){
-            int num1 = 0;
-            int num2 = 0;
-            for(int j=0; j<i; j++){
-                if(s.charAt(j) == '(')
-                    num1++;
-                else
-                    num2++;
-            }
-            if(num1 == num2){
-                temp[0] = s.substring(0, i);
-                temp[1] = s.substring(i, s.length());
-                break;
-            }
-        }
-        return temp;
-    }
-    
-    public String reverseString(String s){
-        StringBuilder sb = new StringBuilder();
-        for(int i=1; i<s.length()-1; i++){
-            char c = s.charAt(i);
-            if(c == '(')
-                sb.append(')');
-            else
-                sb.append('(');
-        }
-        return sb.toString();
+        return true;
     }
 }
