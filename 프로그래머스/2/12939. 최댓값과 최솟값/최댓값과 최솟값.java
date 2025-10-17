162114a4594be8d14ade836ba3f5fe9f769d37e8
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-18 07:27:59 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 3.91 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] temp = s.split(" ");
-        int[] arr = new int[temp.length];
         
-        for(int i=0; i<arr.length; i++)
-            arr[i] = Integer.parseInt(temp[i]);
-        
-        Arrays.sort(arr);
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append(arr[0]).append(" ").append(arr[arr.length-1]);
-        answer = sb.toString();
+        String[] t = s.split(" ");
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i=0; i<t.length; i++){
+            min.add(Integer.parseInt(t[i]));
+            max.add(Integer.parseInt(t[i]));
+        }
+        answer += min.poll();
+        answer += " ";
+        answer += max.poll();
         
         return answer;
     }
