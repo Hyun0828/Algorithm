ad144e95b9291cdb382c061363fc17489e37b1a7
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-20 09:58:03 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 36.94 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,32 +1,39 @@
 import java.util.*;
 
 class Solution{
-    public int solution(int [][]board){
-        int Y = board.length;
-        int X = board[0].length;
-        int[][] dp = new int[Y][X];
+    public int solution(int[][] board){
+        // int[][] dp = new int[board.length][board[0].length];
+        // for(int i=0; i<board[0].length; i++)
+        //     dp[0][i] = board[0][i];
+        // for(int i=0; i<board.length; i++)
+        //     dp[i][0] = board[i][0];
         
-        for(int i=0; i<X; i++){
-            dp[0][i] = board[0][i];
-        }
-        for(int i=0; i<Y; i++){
-            dp[i][0] = board[i][0];
-        }
-        
-        for(int i=1; i<Y; i++){
-            for(int j=1; j<X; j++){
-                if(board[i][j] == 1)
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+        for(int i=1; i<board.length; i++){
+            for(int j=1; j<board[i].length; j++){
+                if(board[i][j] == 0){
+                    continue;
+                }
+                // board[i][j] == 1
+                // 0이 하나라도 있으면 1이다.
+                if(board[i-1][j-1] == 0 || board[i][j-1] == 0 || board[i-1][j] == 0)
+                    board[i][j] = 1;
+                // 0이 없는데 전부 같으면 n+1이다
+                else if(board[i-1][j-1] == board[i-1][j] && board[i-1][j-1] == board[i][j-1])
+                    board[i][j] = board[i-1][j-1] + 1;
+                // 0이 없는데 전부 같진 않으면 최솟값 + 1이다
+                else {
+                    int n = Math.min(board[i-1][j-1], Math.min(board[i][j-1], board[i-1][j]));
+                    board[i][j] = n + 1;
+                }
             }
         }
         
-        int max = 0;
-        for(int i=0; i<Y; i++){
-            for(int j=0; j<X; j++){
-                max = Math.max(max, dp[i][j]);
+        int answer = 0;
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[i].length; j++){
+                answer = Math.max(answer, board[i][j]);
             }
         }
-        
-        return max * max;
+        return answer * answer;
     }
 }
