f1e3a1040329ee41bdbea2284010aa953b8137b8
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-22 10:06:29 +0900

    [level 2] Title: 우박수열 정적분, Time: 5.60 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,58 +1,62 @@
 import java.util.*;
 
 class Solution {
+    
+    List<Long> arr = new ArrayList<>();
+    
     public double[] solution(int k, int[][] ranges) {
         double[] answer = new double[ranges.length];
         
-        double[] result = collatz(k);
-        int n = result.length;
+        collatz(k);
+        int n = arr.size() - 1;
+        
+        double[] nsum = new double[n];
+        for(int i=0; i<n; i++){
+            long y1 = arr.get(i);
+            long y2 = arr.get(i+1);
+            nsum[i] = (double) (y1 + y2) / 2;
+        }
+        for(int i=1; i<n; i++){
+            nsum[i] += nsum[i-1];
+        }
+        for(int i=0; i<n; i++)
+            System.out.println(nsum[i]);
         
         for(int i=0; i<ranges.length; i++){
             int a = ranges[i][0];
             int b = ranges[i][1];
             b = n + b;
             
-            if(a > b){
-                answer[i] = -1;
+            if(a > b){ 
+                answer[i] = -1.0;
             } else if(a == b){
-                answer[i] = 0;
+                answer[i] = 0.0;
             } else {
                 if(b > 0)
                     b--;
-                if(a > 0){
+                if(a > 0) {
                     a--;
-                    answer[i] = result[b] - result[a];
-                } else if(a==0){
-                    answer[i] = result[b];
+                    answer[i] = nsum[b] - nsum[a];
                 }
-                    
-                
+                else if(a == 0)
+                    answer[i] = nsum[b];
             }
         }
-        
         
         return answer;
     }
     
-    public double[] collatz(int k){
-        List<Double> result = new ArrayList<>();
-        while(k > 1){
-            int prev = k;
-            if(k % 2 == 0){
-                k /= 2;
+    public void collatz(int k){
+        long t = (long) k;
+        arr.add(t);
+        while(t > 1){
+            if(t % 2 == 0){
+                t /= 2;
             } else {
-                k = k * 3 + 1;
+                t *= 3;
+                t += 1;
             }
-            int next = k;
-            
-            result.add((double)(prev+next) / 2);
+            arr.add(t);
         }
-        
-        double[] arr = new double[result.size()];
-        arr[0] = result.get(0);
-        for(int i=1; i<result.size(); i++){
-            arr[i] = arr[i-1] + result.get(i);
-        }
-        return arr;
     }
 }
