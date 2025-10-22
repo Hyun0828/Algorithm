4fb7a9fac414d2129a0d7a0b58b7dcb65a151b4e
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-22 09:39:28 +0900

    [level 2] Title: 하노이의 탑, Time: 7.54 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,39 +1,26 @@
 import java.util.*;
 
 class Solution {
+    List<int[]> result = new ArrayList<>();
     
-    static List<Temp> result;
-        
     public int[][] solution(int n) {
-        result = new ArrayList<>();
+        int m = (int) Math.pow(2, n) - 1;
+        int[][] answer = new int[m][2];
         hanoi(n, 1, 2, 3);
-        int[][] answer = new int[result.size()][2];
         for(int i=0; i<result.size(); i++){
-            answer[i][0] = result.get(i).from;
-            answer[i][1] = result.get(i).to;
+            answer[i] = result.get(i);
         }
-        
         return answer;
     }
     
-    public static void hanoi(int n, int a, int b, int c){
-        if(n == 1) {
-            result.add(new Temp(a, c));
+    public void hanoi(int n, int a, int b, int c){
+        if(n == 1){
+            result.add(new int[]{a, c});
             return;
-        }
+        }   
         
         hanoi(n-1, a, c, b);
         hanoi(1, a, b, c);
         hanoi(n-1, b, a, c);
-    }
-    
-    static class Temp {
-        int from;
-        int to;
-        
-        public Temp(int from, int to){
-            this.from = from;
-            this.to = to;
-        }
     }
 }
