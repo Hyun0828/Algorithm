a761a9996558a74f1f8ff06a3ee2fdfe2433408c
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-25 10:28:27 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 162.80 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,41 +1,45 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] diffs, int[] times, long limit) {
-        int answer = 0;
-        
-        int start = 1;
-        int end = 100000;
     
+    int answer = 400000;
+    
+    public int solution(int[] diffs, int[] times, long limit) {
+        // 숙련도의 범위 : 1 or diffs의 최솟값 - 1 ~ diffs의 최댓값
+        // 숙련도가 크면 클수록 당연히 총 소요시간은 짧아진다.
+        // 완전탐색 300,000 * 300,000 니까 시간초과
+        // Parametric Search?
+        // O(log300,000 * 300,000)
+        
+        int[] temps = new int[diffs.length];
+        for(int i=0; i<temps.length; i++)
+            temps[i] = diffs[i];
+        Arrays.sort(temps);
+        parametricSearch(diffs, times, limit, temps[0], temps[temps.length-1]);
+        return answer;
+    }
+    
+    public void parametricSearch(int[] diffs, int[] times, long limit, int start, int end){
         while(start <= end){
             int mid = (start + end) / 2;
+            long time = 0L;
             
-            // f가 true면 저장하고 왼쪽으로, false면 그냥 오른쪽으로
-            if(f(diffs, times, limit, mid)){
-                answer = mid;
-                end =  mid - 1;
+            for(int i=0; i<diffs.length; i++){
+                if(diffs[i] > mid){
+                    int n = diffs[i] - mid;
+                    time += (times[i-1] + times[i]) * n;
+                    time += times[i];
+                } else {
+                    time += times[i];
+                }
+            }
+            
+            if(time <= limit){
+                answer = Math.min(answer, mid);
+                end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
-        
-        return answer;
-    }
-    
-    public boolean f(int[] diffs, int[] times, long limit, int level){
-        long temp = 0 ;
-        for(int i=0; i<diffs.length; i++){
-            if(diffs[i] <= level){
-                temp += times[i];
-            } else {
-                temp += (times[i-1] + times[i]) * (long) (diffs[i] - level);
-                temp += times[i];
-            }
-        
-        }
-        
-        if(limit < temp)
-            return false;
-        return true;
     }
 }
