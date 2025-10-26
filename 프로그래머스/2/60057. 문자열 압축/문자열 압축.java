cbd6a0eeaadf73ed6720acb9cf814841908c61e6
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-26 23:04:28 +0900

    [level 2] Title: 문자열 압축, Time: 4.76 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -2,32 +2,45 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = Integer.MAX_VALUE;
-        int i, j, k;
+        int answer = s.length();
+        int len = s.length();
         
-        for(i = 1; i <= s.length(); i++){
-            String res = "";
-            for(j = 0; j < s.length(); ){
-                String sub = s.substring(j, Math.min(j + i, s.length()));
-                int sum = 1;
-                
-                k = j + i;
-                while(k + i <= s.length()){
-                    String n = s.substring(k, k + i);
-                    if(sub.equals(n)){
-                        sum++;
-                        k += i;
-                    } else {
-                        break;
-                    }   
-                }
-                if(sum > 1)
-                    res += sum;
-                res += sub;
-                j = k;
-            }
-            answer = Math.min(answer, res.length());
+        for(int i=1; i<=len/2; i++){
+            answer = Math.min(answer, comp(s, i));
         }
+        
         return answer;
+    }
+    
+    public int comp(String s, int len){
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<s.length()-len; i+=len){
+            String s1 = s.substring(i, i+len);
+            int count = 1;
+            int j = i+len;
+            
+            for(j=i+len; j<s.length(); j+=len){
+                if(j+len > s.length())
+                    break;
+                String s2 = s.substring(j, j+len);
+                if(s1.equals(s2)){
+                    count++;
+                    i = j;
+                } else {
+                    break;
+                }
+            }
+            
+            if(count > 1)
+                sb.append(count);
+            sb.append(s1);
+            
+            if(j != s.length() && j+len >= s.length()){
+                sb.append(s.substring(j));
+            }
+        }
+        
+        return sb.toString().length();
     }
 }
