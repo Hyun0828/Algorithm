d3adc4533eb6eda0ccbdb4b539e646b153b8b2ea
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-27 15:47:57 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.08 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -2,33 +2,42 @@ import java.util.*;
 
 class Solution {
     public long solution(int w, int h) {
-        long answer = 0;
-        int g = gcd(Math.max(w, h), Math.min(w, h));
-        int nw = w / g;
-        int nh = h / g;
-        if(g == 1){
-            answer += nw + nh - 1; 
-        } else {
-            int tmp = nw + nh - 1;
-            answer += tmp * g;
-        }
+        long answer = 1;
         
-        return (long)w * h - answer;
+        // 안 잘린 정사각형의 개수를 구하시오.
+        
+        // 1. w==h이면, answer = wxh - w
+        // 2. w나 h중 하나가 1이면 answer = 0
+        // 3. w와 h가 서로소이면 w+h-1개 제외
+        
+        // w h 몇개제외?
+        // 2 3 4
+        // 2 4 4
+        // 2 5 6
+        // 2 6 6
+        // 2 7 8
+        // 2 8 8
+        
+        // 3 4 6
+        // 3 5 7
+        // 3 6 8
+        // 3 7 9
+        // 3 8 10
+        
+        int g = gcd(w, h);
+        int gw = w / g;
+        int gh = h / g;
+        answer = (long)w * h - (long)(gw + gh - 1) * g;
+        
+        return answer;
     }
     
     public int gcd(int a, int b){
-        int n = a;
-        int m = b;
-        
-        while(true){
-            int r = n % m;
-            if(r==0){
-                break;
-            }
-            n = m;
-            m = r;
+        while(b!=0){
+            int r = a % b;
+            a = b;
+            b = r;
         }
-        
-        return m;
+        return a;
     }
 }
