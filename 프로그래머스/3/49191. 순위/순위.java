8a0d08f62912fb193778e9626a85da9aa88d1f78
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-30 09:03:37 +0900

    [level 3] Title: 순위, Time: 19.08 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -4,33 +4,38 @@ class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
         
+        // 그래프로 만들었을 때 순위가 어떻게 정해지지?
+        // 단방향 + 방향 그래프
+        
         int[][] graph = new int[n+1][n+1];
         for(int i=0; i<results.length; i++){
             graph[results[i][0]][results[i][1]] = 1;
         }
-        
         for(int k=1; k<=n; k++){
             for(int i=1; i<=n; i++){
                 for(int j=1; j<=n; j++){
-                    if(graph[i][k] == 1 && graph[k][j] == 1){
+                    if(graph[i][k] == 1 && graph[k][j] == 1)
                         graph[i][j] = 1;
-                    }
                 }
             }
         }
-        
         for(int i=1; i<=n; i++){
             int count = 0;
             for(int j=1; j<=n; j++){
-                if(i == j)
-                    continue;
-                if(graph[i][j] == 1 || graph[j][i] == 1){
+                if(graph[i][j] == 1 || graph[j][i] == 1)
                     count++;
-                }
             }
             if(count == n - 1)
                 answer++;
         }
+        
+        //   1 2 3 4 5
+        // 1   1     1
+        // 2         1
+        // 3   1     1
+        // 4   1 1   1
+        // 5     
+        
         
         return answer;
     }
