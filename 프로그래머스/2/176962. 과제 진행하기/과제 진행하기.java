c51725eb53b00bd8181f194afd14f17d649e31ae
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-31 10:27:08 +0900

    [level 2] Title: 과제 진행하기, Time: 12.44 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -5,139 +5,68 @@ class Solution {
         String[] answer = new String[plans.length];
         int idx = 0;
         
-        PriorityQueue<Homework> queue = new PriorityQueue<>();
-        Stack<Homework> stack = new Stack<>();
-        int total_playtime = 0;
-        int total_starttime = 0;
+        PriorityQueue<Plan> pq = new PriorityQueue<>((a,b) -> a.start - b.start);
+        Stack<Plan> stack = new Stack<>();
         
         for(int i=0; i<plans.length; i++){
-            String[] plan = plans[i];
-            total_playtime += Integer.parseInt(plan[2]);
-            queue.add(new Homework(plan[0], timeToint(plan[1]), Integer.parseInt(plan[2])));
+            pq.add(new Plan(plans[i][0], convertToInt(plans[i][1]), Integer.parseInt(plans[i][2])));
         }
-        total_starttime = queue.peek().start;
         
-        // Q. 과제를 하지 않는 시간이 있을 수도 있다.
-        
-        // 전체 과제 진행 시간 동안
-        Homework hw = queue.poll();
-        int time = total_starttime;
-        while(hw.playtime!=0 || !queue.isEmpty() || !stack.isEmpty()){
-            time++;
-            hw.playtime--;
-            
-            // 하고 있는 과제를 다했는데
-            if(hw.playtime == 0){
-                answer[idx++] = hw.subject;
-                
-                if(!queue.isEmpty()){
-                    Homework newHw = queue.peek();
-                    // 새로운 과제 시작 시간이면 새로운 과제를 시작한다.
-                    if(newHw.start == time){
-                        // 하고 있는 과제가 아직 남았다면 과제 중단
-                        if(hw.playtime > 0)
-                            stack.push(hw);
-                        hw = queue.poll();
-                    }
-                    // 아니면 멈췄던 과제를 시작한다.
-                    else {
-                        if(!stack.isEmpty())
-                            hw = stack.pop();   
-                    }
-                }
-                // 남은 새로운 과제가 없으면 멈췄던 과제를 시작한다.
-                else {
-                    if(!stack.isEmpty())
-                        hw = stack.pop();   
-                }
-            }
-            // 과제를 다 못했는데
-            else {
-               if(!queue.isEmpty()){
-                    Homework newHw = queue.peek();
-                    // 새로운 과제 시작 시간이면 새로운 과제를 시작한다.
-                    if(newHw.start == time){
-                        // 하고 있는 과제가 아직 남았다면 과제 중단
-                        if(hw.playtime > 0)
-                            stack.push(hw);
-                        hw = queue.poll();
-                    }    
+        Plan currentPlan = pq.poll();
+        int currentTime = currentPlan.start;
+        while(idx < answer.length){
+            if(!pq.isEmpty()){
+                Plan nextPlan = pq.peek();
+                // 현재 과제를 못 끝냈는데 다음 과제를 시작해야 하면
+                if(currentPlan.start + currentPlan.time > nextPlan.start){
+                    currentPlan.time -= (nextPlan.start - currentPlan.start);
+                    currentPlan.start = nextPlan.start; 
+                    stack.push(new Plan(currentPlan.name, currentPlan.start, currentPlan.time));
+    
+                    currentTime = nextPlan.start;   
+                    currentPlan = pq.poll();
                 } 
-            }   
+                // 현재 과제를 다음 과제 시작 전에 끝낼 수 있으면 + 대기 과제를 먼저 실행하는 경우
+                else if(currentPlan.start + currentPlan.time < nextPlan.start){
+                    answer[idx++] = currentPlan.name;
+                    currentTime = currentPlan.start + currentPlan.time;
+                    if(!stack.isEmpty()) {
+                        currentPlan = stack.pop();
+                        currentPlan.start = currentTime;
+                    } else
+                        currentPlan = pq.poll();
+                } else {
+                    answer[idx++] = currentPlan.name;
+                    currentTime = currentPlan.start + currentPlan.time;
+                    currentPlan = pq.poll();
+                }
+            } else {
+                answer[idx++] = currentPlan.name; // 과제가 대기 큐에만 있으면 지금 하던걸 끝낸다.
+                if(stack.isEmpty())
+                    break;
+                currentPlan = stack.pop();
+            }
         }
-        
-//         for(int i=total_starttime+1; i<=total_starttime+total_playtime; i++){
-            
-//             hw.playtime--;
-            
-//             // 하고 있는 과제를 다했는데
-//             if(hw.playtime == 0){
-//                 answer[idx++] = hw.subject;
-                
-//                 if(!queue.isEmpty()){
-//                     Homework newHw = queue.peek();
-//                     // 새로운 과제 시작 시간이면 새로운 과제를 시작한다.
-//                     if(newHw.start == i){
-//                         // 하고 있는 과제가 아직 남았다면 과제 중단
-//                         if(hw.playtime > 0)
-//                             stack.push(hw);
-//                         hw = queue.poll();
-//                     }
-//                     // 아니면 멈췄던 과제를 시작한다.
-//                     else {
-//                         if(!stack.isEmpty())
-//                             hw = stack.pop();   
-//                     }
-//                 }
-//                 // 남은 새로운 과제가 없으면 멈췄던 과제를 시작한다.
-//                 else {
-//                     if(!stack.isEmpty())
-//                         hw = stack.pop();   
-//                 }
-//             }
-//             // 과제를 다 못했는데
-//             else {
-//                if(!queue.isEmpty()){
-//                     Homework newHw = queue.peek();
-//                     // 새로운 과제 시작 시간이면 새로운 과제를 시작한다.
-//                     if(newHw.start == i){
-//                         // 하고 있는 과제가 아직 남았다면 과제 중단
-//                         if(hw.playtime > 0)
-//                             stack.push(hw);
-//                         hw = queue.poll();
-//                     }    
-//                 } 
-//             }   
-//         }
         
         return answer;
     }
     
-    private int timeToint(String time){
-        String[] temp = time.split(":");
-        int sum = 0;
+    public int convertToInt(String time){
+        String[] t = time.split(":");
+        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+    }
+    
+    public static class Plan {
+        String name;
+        int start;
+        int time;
         
-        sum += Integer.parseInt(temp[0]) * 60;
-        sum += Integer.parseInt(temp[1]);
-        
-        return sum;
-    }
-}
-
-class Homework implements Comparable<Homework>{
-    
-    String subject;
-    int start;
-    int playtime;
-    
-    public Homework(String subject, int start, int playtime){
-        this.subject = subject;
-        this.start = start;
-        this.playtime = playtime;
+        public Plan(String name, int start, int time){
+            this.name = name;
+            this.start = start;
+            this.time = time;
+        }
     }
     
-    @Override
-    public int compareTo(Homework h){
-        return this.start - h.start;
-    }
+    
 }
