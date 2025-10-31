d70ad3a06965f8d6b7ab8439c52faeab71b38071
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-31 09:25:14 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,32 +1,25 @@
 with t as (
     select
         history_id,
+        car_type,
         daily_fee,
-        datediff(end_date, start_date) + 1 as days,
-        case 
+        datediff(end_date, start_date) + 1 as total_date,
+        case
             when datediff(end_date, start_date) + 1 < 7 then '7일 미만'
             when datediff(end_date, start_date) + 1 < 30 then '7일 이상'
             when datediff(end_date, start_date) + 1 < 90 then '30일 이상'
             else '90일 이상'
-        end as duration
-    from car_rental_company_rental_history h
-    join car_rental_company_car c on h.car_id = c.car_id
+        end as duration_type
+    from car_rental_company_car c
+    join car_rental_company_rental_history h on c.car_id = h.car_id
     where c.car_type = '트럭'
-),
-k as (
-    select 
-        duration_type,
-        discount_rate
-    from car_rental_company_discount_plan p
-    where p.car_type = '트럭'
 )
-select  
+select
     history_id,
     case
-        when t.duration = '7일 미만' then daily_fee * days
-        else round(daily_fee * (1 - discount_rate / 100), 0) * days
+        when t.duration_type = '7일 미만' then daily_fee * total_date
+        else round(daily_fee * (1 - discount_rate / 100) * total_date)
     end as fee
 from t
-left join k on t.duration = k.duration_type
+left join car_rental_company_discount_plan d on t.car_type = d.car_type and t.duration_type = d.duration_type
 order by fee desc, history_id desc
-
