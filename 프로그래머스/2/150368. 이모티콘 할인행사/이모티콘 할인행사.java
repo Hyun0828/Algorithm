0388fa17238de0b0f97b4113edb3a300434496af
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-10-31 10:53:47 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 49.41 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,60 +1,71 @@
+import java.util.*;
+
 class Solution {
     
-    int[] arr;
-    int subscribe = 0;
-    int price = 0;
+    PriorityQueue<Point> pq = new PriorityQueue<>();
     
     public int[] solution(int[][] users, int[] emoticons) {
+        int[] answer = new int[2];
         
-        arr = new int[emoticons.length];
+        dfs(users, emoticons, emoticons.length, new int[emoticons.length], 0);
         
-        DFS(0, users, emoticons);
-        
-        int[] answer = {subscribe, price};
+        answer[0] = pq.peek().user;
+        answer[1] = pq.peek().money;
         return answer;
     }
     
-    private void DFS(int idx, int[][] users, int[] emoticons){
-        
-        if(idx == emoticons.length){
-            calculate(users, emoticons);
-            return;
-        }
-        
-        for(int i=10; i<=40; i+=10){
-            arr[idx] = i;
-            DFS(idx+1, users, emoticons);
-        }
-    }
-    
-    private void calculate(int[][] users, int[] emoticons){
-        int count = 0;
-        int money = 0;
+    public void check(int[][] users, int[] emoticons, int[] discount){
+        int gaip = 0;
+        int totalSum = 0;
         
         for(int i=0; i<users.length; i++){
-            int user_percent = users[i][0];
-            int user_price = users[i][1];
+            int rate = users[i][0];
+            int price = users[i][1];
             int sum = 0;
-            
-            for(int j=0; j<arr.length; j++){
-                if(user_percent <= arr[j]){
-                    sum += emoticons[j] * (1-arr[j]*0.01);    
+            for(int j=0; j<emoticons.length; j++){
+                // 일정 비율 이상 할인하는 이모티콘만 구매한다.
+                if(discount[j] >= rate){
+                    sum += emoticons[j] * (100 - discount[j]) / 100;
                 }
             }
             
-            if(user_price <= sum)
-                count++;
-            else
-                money+=sum;
+            // 이모티콘 구매 비용이 price 이상이 되면, 플러스 서비스에 가입한다.
+            if(sum >= price){
+                gaip++;
+            } else {
+                totalSum += sum;
+            }
         }
         
-        if(subscribe < count){
-            subscribe = count;
-            price = money;
+        pq.add(new Point(gaip, totalSum));
+    }
+    
+    public void dfs(int[][] users, int[] emoticons, int m, int[] discount, int count){
+        if(count == m){
+            check(users, emoticons, discount);
             return;
-        } else if(subscribe == count){
-            if(price < money)
-                price = money;
+        }
+        
+        for(int j=1; j<=4; j++){
+            discount[count] = j * 10;
+            dfs(users, emoticons, m, discount, count + 1);
+        }
+    }
+    
+    public static class Point implements Comparable<Point>{
+        int user;
+        int money;
+        
+        public Point(int user, int money){
+            this.user = user;
+            this.money = money;
+        }
+        
+        @Override
+        public int compareTo(Point p){
+            if(this.user == p.user)
+                return p.money - this.money;
+            return p.user - this.user;
         }
     }
 }
