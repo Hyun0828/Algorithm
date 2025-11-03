05728d4729fdbfccb11490e316310d5cf87da6df
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-11-03 19:56:19 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 124.17 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -2,119 +2,77 @@ import java.util.*;
 
 class Solution {
     
-    static int groupNum;
-    static Map<Integer, Integer> groupSize;
+    int[][] map;
+    boolean[][] visited;
+    Map<Integer, Integer> areaMap = new HashMap<>();
     
     public int solution(int[][] land) {
         int answer = 0;
-        int Y = land.length;
-        int X = land[0].length;
-        // Gas[][] map = new Gas[Y][X];
-        int[][] map = new int[Y][X];
-        boolean[][] visited = new boolean[Y][X];
-        groupNum = 1;
-        groupSize = new HashMap<>();
-        // bfs로 각 석유 그룹의 크기를 각각 구한다.
-        // 시추관이 파고드는 열에서 지나가는 석유 그룹의 크기를 더한다.
-        // 500번 반복한다.
-        // for(int i=0; i<Y; i++){
-            // for(int j=0; j<X; j++){
-                // map[i][j] = new Gas(0, 0);
-            // }
-        // }
         
-        for(int i=0; i<Y; i++){
-            for(int j=0; j<X; j++){
+        int n = land.length;
+        int m = land[0].length;
+        map = new int[n][m];
+        visited = new boolean[n][m];
+        int areaIdx = 1;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(land[i][j] == 1 && !visited[i][j]){
-                    bfs(land, map, visited, Y, X, i, j);
+                    int cnt = bfs(land, n, m, i, j, areaIdx);
+                    areaMap.put(areaIdx, cnt);
+                    areaIdx++;
                 }
             }
         }
-                
-        for(int i=0; i<X; i++){
-            Set<Integer> groups = new HashSet<>();
-            int result = 0;
-            for(int j=0; j<Y; j++){
-                // int group = map[j][i].group;
-                // int value = map[j][i].value;
-                int group = map[j][i];
-                
-                if(group == 0)
-                    continue;
-                if(!groups.contains(group)){
-                    groups.add(group);
-                    // result += value;
-                    result += groupSize.get(group);
+        
+        for(int i=0; i<m; i++){
+            Set<Integer> set = new HashSet<>();
+            int sum = 0;
+            for(int j=0; j<n; j++){
+                if(map[j][i] > 0){
+                    set.add(map[j][i]);
                 }
             }
-            answer = Math.max(answer, result);
+            for(Integer idx : set){
+                sum += areaMap.get(idx);
+            }
+            answer = Math.max(answer, sum);
         }
         
         return answer;
     }
     
-    public static void bfs(int[][] land, int[][] map, boolean[][] visited, int Y, int X, int y, int x){
-        int[] dx = {-1, 1, 0 ,0};
-        int[] dy = {0, 0, 1, -1};
+    public int bfs(int[][] land, int n, int m, int y, int x, int areaIdx){
+        Queue<int[]> queue = new LinkedList<>();
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,1,-1};
         
-        int cnt = 0;
-        List<Point> arr = new ArrayList<>();
-        
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(y, x));
+        queue.add(new int[]{y, x});
+        map[y][x] = areaIdx;
+        visited[y][x] = true;
+        int count = 1;
         
         while(!queue.isEmpty()){
-            Point current = queue.poll();
-            int cy = current.y;
-            int cx = current.x;
-            
-            if(visited[cy][cx])
-                continue;
-            visited[cy][cx] = true;
-            
-            cnt++;
-            arr.add(new Point(cy, cx));
+            int[] cur = queue.poll();
+            int cy = cur[0];
+            int cx = cur[1];
             
             for(int i=0; i<4; i++){
                 int ny = cy + dy[i];
                 int nx = cx + dx[i];
                 
-                if(ny < 0 || ny >= Y || nx < 0 || nx >= X)
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m)
                     continue;
-                if(visited[ny][nx])
-                    continue;
-                if(land[ny][nx] == 0)
+                if(land[ny][nx] == 0 || visited[ny][nx])
                     continue;
                 
-                queue.add(new Point(ny, nx));
+                queue.add(new int[]{ny,nx});
+                map[ny][nx] = areaIdx;
+                visited[ny][nx] = true;
+                count++;
             }
         }
         
-        for(Point p : arr){
-            // map[p.y][p.x] = new Gas(groupNum, cnt);
-            map[p.y][p.x] = groupNum;
-        }        
-        groupSize.put(groupNum, cnt);
-        groupNum++;
+        return count;
     }
-    
-    public static class Point {
-        int y;
-        int x;
-        
-        public Point(int y, int x){
-            this.y=y;
-            this.x=x;
-        }
-    }
-    
-//     public static class Gas {
-//         int group;
-//         int value;
-        
-//         public Gas(int group, int value){
-//             this.group = group;
-//             this.value = value;
-//         }
-//     }
 }
