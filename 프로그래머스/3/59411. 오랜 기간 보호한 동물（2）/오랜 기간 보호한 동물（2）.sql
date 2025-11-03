199802b1d436e3ae8082f424f879dc2234ed6b30
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-11-03 23:02:37 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -3,5 +3,5 @@ select
     i.name
 from animal_ins i
 join animal_outs o on i.animal_id = o.animal_id
-order by datediff(o.datetime, i.datetime) desc
+order by datediff(i.datetime,o.datetime) asc
 limit 2
