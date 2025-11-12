b0ea5072694e585ce67e178c74830419e530bbfa
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-11-12 09:10:21 +0900

    [level 2] Title: N-Queen, Time: 153.70 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,47 +1,38 @@
+import java.util.*;
+
 class Solution {
     
     int answer = 0;
-    int[] queen = new int[12];
-    
     public int solution(int n) {
-        
-        backTracking(0, n);
+        dfs(new int[n], 0, n);
         return answer;
     }
     
-    private boolean check(int row, int col, int n){
-        
-        for(int i=0;i<row;i++){
+    public boolean isOk(int[] queens, int row, int col, int n){
+        for(int i=0; i<row; i++){
+            int r = i;
+            int c = queens[i];
             
-            // 같은 열인지
-            if(queen[i] == col)
+            if(c == col)
                 return false;
             
-            // 오른쪽 대각선
-            if(Math.abs(queen[i]-i+n) == Math.abs(col-row+n))
+            if(Math.abs(r-row) == Math.abs(c-col))
                 return false;
-            // 왼쪽 대각선
-            if(Math.abs(queen[i]+i) == Math.abs(col+row))
-                return false;
-            
-            // if (Math.abs(queen[i] - col) == Math.abs(i - row)) 
-            //     return false;   
         }
-        
         return true;
     }
     
-    private void backTracking(int row, int n){
-        
-        if(row==n){
+    public void dfs(int[] queens, int row, int n){
+        if(row == n){
             answer++;
             return;
         }
         
         for(int i=0; i<n; i++){
-            if(check(row, i, n)){
-                queen[row] = i;   
-                backTracking(row+1, n);
+            // (row, i)에 퀸을 둘 수 있다면
+            if(isOk(queens, row, i, n)){
+                queens[row] = i;
+                dfs(queens, row + 1, n);
             }
         }
     }
--- 프로그래머스/2/12952. N－Queen/README.md
+++ 프로그래머스/2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
 
 ### 성능 요약
 
-메모리: 91.4 MB, 시간: 185.93 ms
+메모리: 93.4 MB, 시간: 153.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 22일 11:43:25
+2025년 11월 12일 09:10:16
 
 ### 문제 설명
 
