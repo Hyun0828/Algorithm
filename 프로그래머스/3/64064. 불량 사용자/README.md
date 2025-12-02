015ba20ba72652db59ed48615046959e47222bd1
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-12-02 19:21:16 +0900

    [level 3] Title: 불량 사용자, Time: 99.48 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 140 MB, 시간: 214.62 ms
+메모리: 121 MB, 시간: 99.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 10일 12:41:44
+2025년 12월 02일 19:21:13
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -2,44 +2,41 @@ import java.util.*;
 
 class Solution {
     
-    String[] userIds;
-    String[] bannedIds;
     Set<Set<String>> result = new HashSet<>();
     
-    public int solution(String[] user_id, String[] banned_id) {
-        userIds = user_id;
-        bannedIds = banned_id;
-        dfs(new HashSet<>(), 0);
+    public int solution(String[] user_id, String[] banned_id) {        
+        dfs(user_id, banned_id, new HashSet<>(), 0);
         return result.size();
     }
     
-    public void dfs(Set<String> set, int depth){
-        if(depth == bannedIds.length){
+    public boolean check(String user, String ban){
+        if(user.length() != ban.length())
+            return false;
+        
+        for(int i=0; i<user.length(); i++){
+            if(ban.charAt(i) != '*' && user.charAt(i) != ban.charAt(i)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public void dfs(String[] user_id, String[] banned_id, Set<String> set, int idx){
+        if(idx == banned_id.length){
             result.add(set);
             return;
         }
         
-        for(int i=0; i<userIds.length; i++){
-            if(set.contains(userIds[i]))
+        for(int i=0; i<user_id.length; i++){
+            if(set.contains(user_id[i]))
                 continue;
             
-            if(check(userIds[i], bannedIds[depth])){
-                set.add(userIds[i]);
-                dfs(new HashSet<>(set), depth+1);
-                set.remove(userIds[i]); 
+            if(check(user_id[i], banned_id[idx])){
+                set.add(user_id[i]);
+                dfs(user_id, banned_id, new HashSet<>(set), idx + 1);
+                set.remove(user_id[i]);
             }
         }
-    }
-    
-    public boolean check(String userId, String bannedId){
-        if(userId.length() != bannedId.length())
-            return false;
-        
-        for(int i=0; i<userId.length(); i++){
-            if(bannedId.charAt(i) != '*' && userId.charAt(i) != bannedId.charAt(i)){
-                return false;
-            }
-        }
-        return true;
     }
 }
