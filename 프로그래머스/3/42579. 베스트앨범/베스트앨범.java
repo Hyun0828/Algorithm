1f7d6bd068ff890098c7529349c50d61f0ad411d
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-12-02 14:48:27 +0900

    [level 3] Title: 베스트앨범, Time: 1.57 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,47 +1,52 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-        
-        // 장르별 재생횟수
-        Map<String, Integer> gMap = new HashMap<>();
-        // 장르 내에서 노래별 재생 횟수
-        Map<String, PriorityQueue<Song>> sMap = new HashMap<>();
-        
+    
+    Map<String, Integer> genreMap = new HashMap<>();
+    Map<String, PriorityQueue<Song>> songMap = new HashMap<>(); 
+    
+    public int[] solution(String[] genres, int[] plays) {  
         for(int i=0; i<genres.length; i++){
+            int songNum = i;
             String genre = genres[i];
             int play = plays[i];
             
-            gMap.put(genre, gMap.getOrDefault(genre, 0) + play);
-            sMap.putIfAbsent(genre, new PriorityQueue<Song>());
-            PriorityQueue<Song> pq = sMap.get(genre);
-            pq.add(new Song(play, i));
-        }
-        
-        PriorityQueue<Genre> gpq = new PriorityQueue<>();
-        for(String s : gMap.keySet()){
-            gpq.add(new Genre(s, gMap.get(s)));
+            genreMap.put(genre, genreMap.getOrDefault(genre, 0) + play);
+            songMap.putIfAbsent(genre, new PriorityQueue<>());
+            PriorityQueue<Song> pq = songMap.get(genre);
+            pq.add(new Song(songNum, play));
         }
         
         List<Integer> result = new ArrayList<>();
-        int idx = 0;
-        while(!gpq.isEmpty()){
-            String genre = gpq.poll().genre;
-            PriorityQueue<Song> spq = sMap.get(genre);
-            int count = 0;
-            while(count < 2 && !spq.isEmpty()){
-                result.add(spq.poll().idx);
-                count++;
+        
+        PriorityQueue<Genre> genreHeap = new PriorityQueue<>();
+        for(String s : genreMap.keySet()){
+            genreHeap.add(new Genre(s, genreMap.get(s)));
+        }
+        
+        while(!genreHeap.isEmpty()){
+            int num = 0;
+            Genre genre = genreHeap.poll();
+            PriorityQueue<Song> pq = songMap.get(genre.genre);
+            while(!pq.isEmpty()){
+                if(num == 2){
+                    break;
+                }
+                Song song = pq.poll();
+                result.add(song.songNum);
+                num++;
             }
         }
         
         int[] answer = new int[result.size()];
-        for(int i=0; i<answer.length; i++)
+        for(int i=0; i<result.size(); i++){
             answer[i] = result.get(i);
+        }
+        
         return answer;
     }
     
-    public static class Genre implements Comparable<Genre> {
+    public static class Genre implements Comparable<Genre>{
         String genre;
         int play;
         
@@ -56,18 +61,22 @@ class Solution {
         }
     }
     
-    public static class Song implements Comparable<Song> {
+    public static class Song implements Comparable<Song>{
+        int songNum;
         int play;
-        int idx;
         
-        public Song(int play, int idx){
+        public Song(int songNum, int play){
+            this.songNum = songNum;
             this.play = play;
-            this.idx = idx;
         }
         
         @Override
         public int compareTo(Song s){
-            return s.play - this.play;
+            if(this.play == s.play){
+                return this.songNum - s.songNum;
+            } else {
+                return s.play - this.play;
+            }
         }
     }
 }
