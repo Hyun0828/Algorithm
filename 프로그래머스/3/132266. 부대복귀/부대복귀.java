353217c2d375c5147c89562b305a3cbf7cce29d6
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-12-03 10:19:52 +0900

    [level 3] Title: 부대복귀, Time: 219.65 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,80 +2,62 @@ import java.util.*;
 
 class Solution {
     
-    List<Edge>[] graph;
-    int[] distance;
+    List<Integer>[] graph;
     boolean[] visited;
-    int INF = Integer.MAX_VALUE;
+    int[] distance;
+    int INF = 100000000;
     
-    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {        
+        init(n, roads);
+        visited[destination] = true;
+        distance[destination] = 0;
+        
+        dijkstra(destination);
+        
         int[] answer = new int[sources.length];
-        
-        graph = new ArrayList[n+1];
-        for(int i=1; i<=n; i++){
-            graph[i] = new ArrayList<>();
-        }
-        distance = new int[n+1];
-        visited = new boolean[n+1];
-        Arrays.fill(distance, INF);
-        
-        for(int i=0; i<roads.length; i++){
-            int a = roads[i][0];
-            int b = roads[i][1];
-            graph[a].add(new Edge(b, 1));
-            graph[b].add(new Edge(a, 1));
-        }
-        
-        dijkstra(n, destination);
-        
-        for(int i=0; i<sources.length; i++){
-            int source = sources[i];
-            if(distance[source] != INF){
-                answer[i] = distance[source];
-            } else {
-                answer[i] = -1; 
-            }
+        for(int i=0; i<answer.length; i++){
+            answer[i] = distance[sources[i]] != INF ? distance[sources[i]] : -1;
         }
         
         return answer;
     }
     
-    public void dijkstra(int n, int start){
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        distance[start] = 0;
-        pq.add(new Edge(start, 0));
+    public void init(int n, int[][] roads){
+        graph = new ArrayList[n+1];
+        for(int i=1; i<=n; i++)
+            graph[i] = new ArrayList<>();
         
-        while(!pq.isEmpty()){
-            Edge cur = pq.poll();
-            int u = cur.to;
-            int w = cur.weight;
+        visited = new boolean[n+1];
+        distance = new int[n+1];
+        Arrays.fill(distance, INF);
+        
+        for(int i=0; i<roads.length; i++){
+            int a = roads[i][0];
+            int b = roads[i][1];
             
-            if(visited[u])
-                continue;
-            visited[u] = true;
-            
-            for(Edge e : graph[u]){
-                int v = e.to;
-                int dis = e.weight;
-                if(!visited[v] && distance[v] > distance[u] + dis){
-                    distance[v] = distance[u] + dis;
-                    pq.add(new Edge(v, distance[v]));
-                }
-            }
+            graph[a].add(b);
+            graph[b].add(a);
         }
     }
     
-    public static class Edge implements Comparable<Edge>{
-        int to;
-        int weight;
+    public void dijkstra(int start){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.add(new int[]{start, 0});
         
-        public Edge(int to, int weight){
-            this.to = to;
-            this.weight = weight;
-        }
-        
-        @Override
-        public int compareTo(Edge e){
-            return this.weight - e.weight;
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int cNode = cur[0];
+            int cDis = cur[1];
+            
+            List<Integer> edges = graph[cNode];
+            for(Integer to : edges){
+                if(visited[to])
+                    continue;
+                if(distance[to] > distance[cNode] + 1){
+                    distance[to] = distance[cNode] + 1;
+                    pq.add(new int[]{to, distance[to]});
+                }
+            }
         }
     }
 }
