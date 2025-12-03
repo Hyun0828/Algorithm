dcab7f522a26a9733c91a799b352da9aa622af04
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-12-03 22:20:00 +0900

    [level 3] Title: 입국심사, Time: 74.21 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -4,14 +4,11 @@ class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         
-        // 특정 시간 이하면 심사를 받을 수 없고, 특정 시간 이상이면 무조건 심사가 가능하다.
-        // parametric search !
-        
         long start = 0;
         long end = 1000000000L * 1000000000L;
         while(start <= end){
             long mid = (start + end) / 2;
-            if(check(n, times, mid)){
+            if(check(mid, n, times)){
                 answer = mid;
                 end = mid - 1;
             } else {
@@ -22,12 +19,12 @@ class Solution {
         return answer;
     }
     
-    public boolean check(int n, int[] times, long time){
-        long num = 0;
+    public boolean check(long mid, int n, int[] times){
+        long sum = 0;
         for(int i=0; i<times.length; i++){
-            num += time / (long) times[i];
+            sum += mid / (long) times[i];
         }
-        if(num >= n)
+        if(sum >= n)
             return true;
         return false;
     }
