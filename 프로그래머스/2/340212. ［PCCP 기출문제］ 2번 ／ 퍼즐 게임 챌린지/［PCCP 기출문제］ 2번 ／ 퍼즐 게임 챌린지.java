f921de7878d8c2d7a1a8beca371d4e48137aed2b
Hyun <71479783+Hyun0828@users.noreply.github.com>
2025-12-04 21:51:51 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 26.69 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,45 +1,37 @@
 import java.util.*;
 
 class Solution {
-    
-    int answer = 400000;
-    
     public int solution(int[] diffs, int[] times, long limit) {
-        // 숙련도의 범위 : 1 or diffs의 최솟값 - 1 ~ diffs의 최댓값
-        // 숙련도가 크면 클수록 당연히 총 소요시간은 짧아진다.
-        // 완전탐색 300,000 * 300,000 니까 시간초과
-        // Parametric Search?
-        // O(log300,000 * 300,000)
+        int answer = 0;
         
-        int[] temps = new int[diffs.length];
-        for(int i=0; i<temps.length; i++)
-            temps[i] = diffs[i];
-        Arrays.sort(temps);
-        parametricSearch(diffs, times, limit, temps[0], temps[temps.length-1]);
-        return answer;
-    }
-    
-    public void parametricSearch(int[] diffs, int[] times, long limit, int start, int end){
+        int start = 1;
+        int end = 100000;
+        
         while(start <= end){
             int mid = (start + end) / 2;
-            long time = 0L;
-            
-            for(int i=0; i<diffs.length; i++){
-                if(diffs[i] > mid){
-                    int n = diffs[i] - mid;
-                    time += (times[i-1] + times[i]) * n;
-                    time += times[i];
-                } else {
-                    time += times[i];
-                }
-            }
-            
-            if(time <= limit){
-                answer = Math.min(answer, mid);
+            if(check(diffs, times, mid, limit)){
+                answer = mid;
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
+        return answer;
+    }
+    
+    public boolean check(int[] diffs, int[] times, int level, long limit){
+        long sum = (long) times[0];
+        for(int i=1; i<diffs.length; i++){
+            if(diffs[i] <= level){
+                sum += times[i];
+            } else {
+                sum += (diffs[i] - level) * (times[i] + times[i-1]);
+                sum += times[i];
+            }
+        }
+        
+        if(sum <= limit)
+            return true;
+        return false;
     }
 }
